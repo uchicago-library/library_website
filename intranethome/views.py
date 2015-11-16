1ef3c6969cce757f842de89c836d6b5b4ad50dfd
Brad Busenius <bbusenius@users.noreply.github.com>
2015-11-16 17:14:39 -0600

    Little patch to prevent intranet pages from blowing up when the database is empty.

--- intranethome/views.py
+++ intranethome/views.py
@@ -9,20 +9,21 @@ from news.models import NewsIndexPage, NewsPage
 import datetime, string
 
 def index(request):
-    committees_and_groups_link = GroupIndexPage.objects.all()[0].url
-    departments_link = IntranetUnitsIndexPage.objects.all()[0].url
-    news_link = NewsIndexPage.objects.all()[0].url
 
-    news_index_page = NewsIndexPage.objects.all()[0]
+    committees_and_groups_link = GroupIndexPage.objects.live()[0].url if GroupIndexPage.objects.exists() else []
+    departments_link = IntranetUnitsIndexPage.objects.live()[0].url if IntranetUnitsIndexPage.objects.exists() else []
+    news_link = NewsIndexPage.objects.live()[0].url if NewsIndexPage.objects.exists() else []
+    news_index_page = NewsIndexPage.objects.live()[0] if NewsIndexPage.objects.exists() else []
 
     news_pages = []
-    for child in news_index_page.get_descendants():
-        # is it that get_descendants gets the page, but not the object? 
-        news_pages.append({
-            'publish_on': NewsPage.objects.get(pk=child.pk).publish_on.strftime('%B %d').replace(' 0', ' '),
-            'title': child.title,
-            'url':   child.url
-        })
+    if NewsIndexPage.objects.exists():
+        for child in news_index_page.get_descendants():
+            # is it that get_descendants gets the page, but not the object?
+            news_pages.append({
+                'publish_on': NewsPage.objects.get(pk=child.pk).publish_on.strftime('%B %d').replace(' 0', ' '),
+                'title': child.title,
+                'url':   child.url
+            })
 
     return render(request, 'intranethome/intranet_home_page.html', {
         'human_resources_link': '#',
