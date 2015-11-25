4386a5df68b544e82effa31604b59110c554f2b7
John Jung <jej@moss.lib.uchicago.edu>
2015-11-25 11:33:48 -0600

    Added a migration for groups.

---
+++ group/migrations/0036_auto_20151125_1733.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+from django.utils.timezone import utc
+import datetime
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('group', '0035_auto_20151125_1427'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='grouppage',
+            name='meeting_end_time',
+            field=models.TimeField(default=datetime.datetime(2015, 11, 25, 18, 33, 17, 721196, tzinfo=utc), blank=True),
+        ),
+    ]
