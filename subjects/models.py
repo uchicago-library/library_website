2f90f87b4bdb060fd304c402a1d2dd6e6bdebff2
Brad Busenius <bbusenius@users.noreply.github.com>
2016-01-20 17:10:35 -0600

    Made the parent field for subject snippets repeatable.

--- subjects/models.py
+++ subjects/models.py
@@ -1,24 +1,35 @@
 from django.db import models
 from wagtail.wagtailsearch import index
 from wagtail.wagtailsnippets.models import register_snippet
-from wagtail.wagtailadmin.edit_handlers import FieldPanel
+from wagtail.wagtailadmin.edit_handlers import FieldPanel, InlinePanel
+from wagtail.wagtailsnippets.edit_handlers import SnippetChooserPanel
+from wagtail.wagtailcore.models import Orderable
+from modelcluster.fields import ParentalKey
+from modelcluster.models import ClusterableModel
+
+class SubjectParentRelations(Orderable, models.Model):
+    """
+    Through table for capturing subjects with multiple
+    parent subjects.
+    """
+    child = ParentalKey('subjects.Subject', related_name='parent_subject') # Yeah, this is weird but it works
+    parent = models.ForeignKey('subjects.Subject', related_name='+')
+
+    panels = [
+        SnippetChooserPanel('parent'),
+    ]
+
 
 @register_snippet
-class Subject(models.Model, index.Indexed):
+class Subject(ClusterableModel, index.Indexed):
     """
     Snippet for subjects.
     """
     name = models.CharField(max_length=255, blank=False)
-    parent = models.ForeignKey(
-        'self',
-        null=True, 
-        blank=True, 
-        on_delete=models.SET_NULL, 
-    )
 
     panels = [
         FieldPanel('name'),
-        FieldPanel('parent'),
+        InlinePanel('parent_subject', label="Parent"),
     ]
 
     def __str__(self):
@@ -27,3 +38,5 @@ class Subject(models.Model, index.Indexed):
     search_fields = [
         index.SearchField('name', partial_match=True),
     ]
+
+
