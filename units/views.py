13560d113a12d7ae02a2b13492cf2e8bee83d938
John Jung <jej@moss.lib.uchicago.edu>
2016-03-23 11:29:28 -0500

    Wired up library pulldown on public directory.

--- units/views.py
+++ units/views.py
@@ -1,7 +1,9 @@
+from directory_unit.models import DirectoryUnit
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.db.models import Q
 from django.shortcuts import render
 from django.utils.html import escape
-from staff.models import StaffPage
+from staff.models import StaffPage, StaffPagePageVCards
 from units.models import UnitPage
 
 def units(request):
@@ -66,55 +68,97 @@ def units(request):
     # 
     # MAIN
     #
+
+    alphabetical_html = ''
+    hierarchical_html = ''
     
+    library = request.GET.get('library', None)
     page = request.GET.get('page', 1)
     query = request.GET.get('query', None)
     sort = request.GET.get('sort', 'alphabetical')
     view = request.GET.get('view', 'department')
 
     # staff pages
-    staff_pages_all = StaffPage.objects.live().order_by('last_name', 'first_name')
+    staff_pages_all = []
     staff_pages = []
 
-    # search staff pages.
-    if query:
-        if staff_pages_all:
-            staff_pages_all = staff_pages_all.search(query)
+    if view == 'staff':
+        staff_pks = []
+        if library:
+            # get a queryset of units from each library building, then get a list of distinct StaffPage pks for each queryset. 
+            if library == 'Eckhart Library':
+                eckhart_units = DirectoryUnit.objects.get(name='Eckhart Library').get_descendants(True)
+                staff_pks = StaffPagePageVCards.objects.filter(unit__in=eckhart_units).values_list('page', flat=True).distinct()
+            elif library == 'Crerar Library':
+                crerar_units = DirectoryUnit.objects.get(name='Science Libraries').get_descendants(True)
+                eckhart_units = DirectoryUnit.objects.get(name='Eckhart Library').get_descendants(True)
+                staff_pks = StaffPagePageVCards.objects.filter(unit__in=crerar_units).exclude(unit__in=eckhart_units).values_list('page', flat=True).distinct()
+            elif library == 'D\'Angelo Law Library':
+                dangelo_units = DirectoryUnit.objects.get(name='D\'Angelo Law Library').get_descendants(True)
+                staff_pks = StaffPagePageVCards.objects.filter(unit__in=dangelo_units).values_list('page', flat=True).distinct()
+            elif library == 'SSA Library':
+                ssa = DirectoryUnit.objects.get(name='Social Service Administration Library (SSA)').get_descendants(True)
+                staff_pks = StaffPagePageVCards.objects.filter(unit__in=ssa).values_list('page', flat=True).distinct()
+            elif library == 'Regenstein Library':
+                crerar = DirectoryUnit.objects.get(name='Science Libraries').get_descendants(True)
+                dangelo = DirectoryUnit.objects.get(name='D\'Angelo Law Library').get_descendants(True)
+                ssa = DirectoryUnit.objects.get(name='Social Service Administration Library (SSA)').get_descendants(True)
+                staff_pks = StaffPagePageVCards.objects.all().exclude(unit__in=crerar).exclude(unit__in=dangelo).exclude(unit__in=ssa).values_list('page', flat=True).distinct()
+            elif library == 'Special Collections Research Center':
+                scrc = DirectoryUnit.objects.get(name='Special Collections Research Center').get_descendants(True)
+                staff_pks = StaffPagePageVCards.objects.all().filter(unit__in=scrc).values_list('page', flat=True).distinct()
+            elif library == 'Mansueto':
+                mansueto = DirectoryUnit.objects.filter(Q(name='Mansueto') | Q(name='Mansueto Library'))
+                staff_pks = StaffPagePageVCards.objects.all().filter(unit__in=mansueto).values_list('page', flat=True).distinct()
+                
+        # get StaffPages themselves from the pk list. 
+        if staff_pks:
+            staff_pages_all = StaffPage.objects.filter(pk__in=staff_pks).order_by('last_name', 'first_name')
         else:
-            staff_pages_all = StaffPage.objects.live().search(query)
-    
-    # add paging.
-    paginator = Paginator(staff_pages_all, 50)
-    try:
-        staff_pages = paginator.page(page)
-    except PageNotAnInteger:
-        staff_pages = paginator.page(1)
-    except EmptyPage:
-        staff_pages = paginator.page(paginator.num_pages)
-    
-    hierarchical_units = UnitPage.hierarchical_units()
-    hierarchical_html = get_html(hierarchical_units)
-    # replace first ul. 
-    if len(hierarchical_html) > 4:
-        hierarchical_html = "<ul class='directory'>" + hierarchical_html[4:]
+            staff_pages_all = StaffPage.objects.live().order_by('last_name', 'first_name')
 
-    # alphabetical units. 
-    alphabetical_html = "<table class='table table-striped'>"
-    for unit_page in UnitPage.objects.filter(display_in_directory=True).extra(select={'lc': 'lower(alphabetical_directory_name)'}).order_by('lc'):
-        alphabetical_html = alphabetical_html + '<tr>'
-        alphabetical_html = alphabetical_html + '<td><strong>' + unit_page.alphabetical_directory_name + '</strong></td>'
-        alphabetical_html = alphabetical_html + '<td>'
-        alphabetical_html = alphabetical_html + get_unit_info_from_unit_page(unit_page)
-        alphabetical_html = alphabetical_html + '</td>'
-        alphabetical_html = alphabetical_html + '<td>'
-        alphabetical_html = alphabetical_html + unit_page.directory_unit.get_parent_library_name()
-        alphabetical_html = alphabetical_html + '</td>'
-        alphabetical_html = alphabetical_html + '</tr>'
-    alphabetical_html = alphabetical_html + '</table>'
+        # search staff pages.
+        if query:
+            if staff_pages_all:
+                staff_pages_all = staff_pages_all.search(query)
+            else:
+                staff_pages_all = StaffPage.objects.live().search(query)
+        
+        # add paging.
+        paginator = Paginator(staff_pages_all, 50)
+        try:
+            staff_pages = paginator.page(page)
+        except PageNotAnInteger:
+            staff_pages = paginator.page(1)
+        except EmptyPage:
+            staff_pages = paginator.page(paginator.num_pages)
+
+    elif view == 'department':
+        hierarchical_units = UnitPage.hierarchical_units()
+        hierarchical_html = get_html(hierarchical_units)
+        # replace first ul. 
+        if len(hierarchical_html) > 4:
+            hierarchical_html = "<ul class='directory'>" + hierarchical_html[4:]
+    
+        # alphabetical units. 
+        alphabetical_html = "<table class='table table-striped'>"
+        for unit_page in UnitPage.objects.filter(display_in_directory=True).extra(select={'lc': 'lower(alphabetical_directory_name)'}).order_by('lc'):
+            alphabetical_html = alphabetical_html + '<tr>'
+            alphabetical_html = alphabetical_html + '<td><strong>' + unit_page.alphabetical_directory_name + '</strong></td>'
+            alphabetical_html = alphabetical_html + '<td>'
+            alphabetical_html = alphabetical_html + get_unit_info_from_unit_page(unit_page)
+            alphabetical_html = alphabetical_html + '</td>'
+            alphabetical_html = alphabetical_html + '<td>'
+            alphabetical_html = alphabetical_html + unit_page.directory_unit.get_parent_library_name()
+            alphabetical_html = alphabetical_html + '</td>'
+            alphabetical_html = alphabetical_html + '</tr>'
+        alphabetical_html = alphabetical_html + '</table>'
 
     return render(request, 'units/unit_index_page.html', {
         'alphabetical_units': alphabetical_html,
         'hierarchical_units': hierarchical_html,
+        'libraries': ["Regenstein Library", "Crerar Library", "D'Angelo Law Library", "Eckhart Library", "Mansueto", "Special Collections Research Center", "SSA Library"],
+        'library': library,
         'query': query,
         'sort': sort,
         'staff_pages': staff_pages,
