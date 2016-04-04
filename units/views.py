2fe71d865d71b43aca0a9b8ba9f841437485eed8
John Jung <jej@moss.lib.uchicago.edu>
2016-04-04 16:59:36 -0500

    Directory updates.

--- units/views.py
+++ units/views.py
@@ -7,6 +7,58 @@ from staff.models import StaffPage, StaffPagePageVCards, StaffPageSubjectPlaceme
 from subjects.models import Subject
 from units.models import UnitPage
 
+def get_subjects():
+    subject_pks = StaffPageSubjectPlacement.objects.all().values_list('subject', flat=True).distinct()
+    subjects = Subject.objects.filter(pk__in=subject_pks).values_list('name', flat=True)
+    return subjects
+
+def get_departments():
+    departments = []
+    for d in DirectoryUnit.objects.filter(parentUnit = DirectoryUnit.objects.get(parentUnit = None)):
+        departments.append(d.fullName)
+    return departments
+
+def get_vcards_for_department(department):
+    depts = DirectoryUnit.objects.get(fullName=department).get_descendants(True)
+    vcards = VCard.objects.filter(unit__in=depts)
+    return vcards
+
+def get_staff_pages_for_library(library = None):
+    staff_pks = []
+    if library:
+        # get a queryset of units from each library building, then get a list of distinct StaffPage pks for each queryset. 
+        if library == 'Eckhart Library':
+            eckhart_units = DirectoryUnit.objects.get(name='Eckhart Library').get_descendants(True)
+            staff_pks = StaffPagePageVCards.objects.filter(unit__in=eckhart_units).values_list('page', flat=True).distinct()
+        elif library == 'Crerar Library':
+            crerar_units = DirectoryUnit.objects.get(name='Science Libraries').get_descendants(True)
+            eckhart_units = DirectoryUnit.objects.get(name='Eckhart Library').get_descendants(True)
+            staff_pks = StaffPagePageVCards.objects.filter(unit__in=crerar_units).exclude(unit__in=eckhart_units).values_list('page', flat=True).distinct()
+        elif library == 'D\'Angelo Law Library':
+            dangelo_units = DirectoryUnit.objects.get(name='D\'Angelo Law Library').get_descendants(True)
+            staff_pks = StaffPagePageVCards.objects.filter(unit__in=dangelo_units).values_list('page', flat=True).distinct()
+        elif library == 'SSA Library':
+            ssa = DirectoryUnit.objects.get(name='Social Service Administration Library (SSA)').get_descendants(True)
+            staff_pks = StaffPagePageVCards.objects.filter(unit__in=ssa).values_list('page', flat=True).distinct()
+        elif library == 'Regenstein Library':
+            crerar = DirectoryUnit.objects.get(name='Science Libraries').get_descendants(True)
+            dangelo = DirectoryUnit.objects.get(name='D\'Angelo Law Library').get_descendants(True)
+            ssa = DirectoryUnit.objects.get(name='Social Service Administration Library (SSA)').get_descendants(True)
+            staff_pks = StaffPagePageVCards.objects.all().exclude(unit__in=crerar).exclude(unit__in=dangelo).exclude(unit__in=ssa).values_list('page', flat=True).distinct()
+        elif library == 'Special Collections Research Center':
+            scrc = DirectoryUnit.objects.get(name='Special Collections Research Center').get_descendants(True)
+            staff_pks = StaffPagePageVCards.objects.all().filter(unit__in=scrc).values_list('page', flat=True).distinct()
+        elif library == 'Mansueto':
+            mansueto = DirectoryUnit.objects.filter(Q(name='Mansueto') | Q(name='Mansueto Library'))
+            staff_pks = StaffPagePageVCards.objects.all().filter(unit__in=mansueto).values_list('page', flat=True).distinct()
+
+    # get StaffPages themselves from the pk list. 
+    if staff_pks:
+        staff_pages_all = StaffPage.objects.filter(pk__in=staff_pks).order_by('last_name', 'first_name')
+    else:
+        staff_pages_all = StaffPage.objects.live().order_by('last_name', 'first_name')
+    return staff_pages_all
+
 def units(request):
     def get_unit_info_from_unit_page(unit_page):
         h = ''
@@ -81,59 +133,18 @@ def units(request):
     subject = request.GET.get('subject', None)
     view = request.GET.get('view', 'department')
 
-    # subjects
-    subject_pks = StaffPageSubjectPlacement.objects.all().values_list('subject', flat=True).distinct()
-    subjects = Subject.objects.filter(pk__in=subject_pks).values_list('name', flat=True)
-
-    # departments
-    departments = []
-    for d in DirectoryUnit.objects.filter(parentUnit = DirectoryUnit.objects.get(parentUnit = None)):
-        departments.append(d.fullName)
-
     # staff pages
     staff_pages_all = []
     staff_pages = []
 
     if view == 'staff':
-        staff_pks = []
-        if library:
-            # get a queryset of units from each library building, then get a list of distinct StaffPage pks for each queryset. 
-            if library == 'Eckhart Library':
-                eckhart_units = DirectoryUnit.objects.get(name='Eckhart Library').get_descendants(True)
-                staff_pks = StaffPagePageVCards.objects.filter(unit__in=eckhart_units).values_list('page', flat=True).distinct()
-            elif library == 'Crerar Library':
-                crerar_units = DirectoryUnit.objects.get(name='Science Libraries').get_descendants(True)
-                eckhart_units = DirectoryUnit.objects.get(name='Eckhart Library').get_descendants(True)
-                staff_pks = StaffPagePageVCards.objects.filter(unit__in=crerar_units).exclude(unit__in=eckhart_units).values_list('page', flat=True).distinct()
-            elif library == 'D\'Angelo Law Library':
-                dangelo_units = DirectoryUnit.objects.get(name='D\'Angelo Law Library').get_descendants(True)
-                staff_pks = StaffPagePageVCards.objects.filter(unit__in=dangelo_units).values_list('page', flat=True).distinct()
-            elif library == 'SSA Library':
-                ssa = DirectoryUnit.objects.get(name='Social Service Administration Library (SSA)').get_descendants(True)
-                staff_pks = StaffPagePageVCards.objects.filter(unit__in=ssa).values_list('page', flat=True).distinct()
-            elif library == 'Regenstein Library':
-                crerar = DirectoryUnit.objects.get(name='Science Libraries').get_descendants(True)
-                dangelo = DirectoryUnit.objects.get(name='D\'Angelo Law Library').get_descendants(True)
-                ssa = DirectoryUnit.objects.get(name='Social Service Administration Library (SSA)').get_descendants(True)
-                staff_pks = StaffPagePageVCards.objects.all().exclude(unit__in=crerar).exclude(unit__in=dangelo).exclude(unit__in=ssa).values_list('page', flat=True).distinct()
-            elif library == 'Special Collections Research Center':
-                scrc = DirectoryUnit.objects.get(name='Special Collections Research Center').get_descendants(True)
-                staff_pks = StaffPagePageVCards.objects.all().filter(unit__in=scrc).values_list('page', flat=True).distinct()
-            elif library == 'Mansueto':
-                mansueto = DirectoryUnit.objects.filter(Q(name='Mansueto') | Q(name='Mansueto Library'))
-                staff_pks = StaffPagePageVCards.objects.all().filter(unit__in=mansueto).values_list('page', flat=True).distinct()
-
-        # get StaffPages themselves from the pk list. 
-        if staff_pks:
-            staff_pages_all = StaffPage.objects.filter(pk__in=staff_pks).order_by('last_name', 'first_name')
-        else:
-            staff_pages_all = StaffPage.objects.live().order_by('last_name', 'first_name')
+        # returns all staff pages if library is None.
+        # otherwise, returns staff pages for the given library.
+        staff_pages_all = get_staff_pages_for_library(library)
 
         # departments.
         if department:
-            depts = DirectoryUnit.objects.get(fullName=department).get_descendants(True)
-            vcards = VCard.objects.filter(unit__in=depts)
-            staff_pages_all = staff_pages_all.filter(vcards__in=vcards)
+            staff_pages_all = staff_pages_all.filter(vcards__in=get_vcards_for_department(department))
 
         # search staff pages.
         if query:
@@ -172,14 +183,14 @@ def units(request):
     return render(request, 'units/unit_index_page.html', {
         'alphabetical_units': alphabetical_html,
         'department': department,
-        'departments': departments,
+        'departments': get_departments(),
         'hierarchical_units': hierarchical_html,
         'libraries': ["Regenstein Library", "Crerar Library", "D'Angelo Law Library", "Eckhart Library", "Mansueto", "Special Collections Research Center", "SSA Library"],
         'library': library,
         'query': query,
         'sort': sort,
         'staff_pages': staff_pages,
-        'subjects': subjects,
+        'subjects': get_subjects(),
         'subject': subject,
         'view': view
     })
