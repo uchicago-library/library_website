3476ebbea787115c237d814485bcfff28a0ab8cc
John Jung <jej@moss.lib.uchicago.edu>
2016-04-28 10:23:43 -0500

    collections/exhibits/subjects browse- started adding formats and subjects.

--- lib_collections/views.py
+++ lib_collections/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
+from lib_collections.models import CollectionPage, CollectionPageFormatPlacement, Format
 from subjects.models import Subject
 
 def collections(request):
-    view = request.GET.get('view', 'collections')
+    # formats
+    collection_placement_ids = CollectionPage.objects.all().values_list('collection_placements', flat=True)
+    collection_page_format_placement_ids = CollectionPageFormatPlacement.objects.filter(id__in=collection_placement_ids)
+    format_ids = CollectionPageFormatPlacement.objects.filter(id__in=collection_page_format_placement_ids).values_list('format', flat=True).distinct()
+    formats = Format.objects.filter(id__in=format_ids).values_list('text', flat=True)
+    formats = sorted(formats)
 
     subjects = []
     # this needs to fold the see alsos in. 
@@ -16,8 +22,18 @@ def collections(request):
                 'name': see_also.alias,
                 'see_also': see_also.snippet.name
             })
+    subjects = sorted(subjects, key=lambda s: s['name'])
+
+    # PARAMETERS
+    format = request.GET.get('format', None)
+    if not format in formats:
+        format = None
+    view = request.GET.get('view', 'collections')
 
     return render(request, 'lib_collections/collections_index_page.html', {
+        'collections': CollectionPage.objects.all(),
+        'format': format,
+        'formats': formats,
         'subjects': subjects,
         'view': view
     })
