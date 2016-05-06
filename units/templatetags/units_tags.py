a4fc8a6cbb76540222004cf1b7c2f9c7cb573026
John Jung <jej@moss.lib.uchicago.edu>
2016-05-06 11:00:11 -0500

    Added library building logic to facex/phone template tag.

--- units/templatetags/units_tags.py
+++ units/templatetags/units_tags.py
@@ -1,4 +1,5 @@
 from django import template
+from public.models import LocationPage
 
 register = template.Library()
 
@@ -14,9 +15,39 @@ def staff_faculty_exchanges_phone_numbers(staff_page):
     for vcard in staff_page.vcards.all():
         facex_phone_pairs.add(vcard.faculty_exchange + '\t' + vcard.phone_number)
     
-    facex_phone_pairs_list = sorted(list(map(lambda p: p.split('\t'), list(facex_phone_pairs))), key=lambda p: p[0])
+    facex_phone_pairs_list = list(map(lambda p: p.split('\t'), list(facex_phone_pairs)))
+
+    lib_room_phone = []
+    for p in facex_phone_pairs_list:
+        facex_parts = p[0].split(' ')
+       
+        lib = '' 
+        if facex_parts[0] == 'JCL':     
+            lib = LocationPage.objects.get(title='The John Crerar Library')
+        elif facex_parts[0] == 'JRL':
+            lib = LocationPage.objects.get(title='The Joseph Regenstein Library')
+        elif facex_parts[0] == 'LBQ':
+            lib = None
+        elif facex_parts[0] in ['MAN', 'Mansueto']:
+            lib = LocationPage.objects.get(title='The Joe and Rika Mansueto Library')
+        elif facex_parts[0] == 'SSA':
+            lib = LocationPage.objects.get(title='SSA Library')
+        else:
+            lib = None
+
+        if len(facex_parts) > 1:
+            room = facex_parts[1]
+        else:
+            room = None
+
+        if p[1]:
+            phone = p[1]
+        else:
+            phone = None
+       
+        lib_room_phone.append([lib, room, phone])
     
     return {
-        'facex_phone_pairs': facex_phone_pairs_list
+        'lib_room_phone': lib_room_phone
     }
     
