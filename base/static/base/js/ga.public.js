e08dc5a2d9e8ea5b2d3b6c13ab8897eccc77891f
John Jung <jej@moss.lib.uchicago.edu>
2016-05-19 14:50:55 -0500

    Updated Google Analytics.

--- base/static/base/js/ga.public.js
+++ base/static/base/js/ga.public.js
@@ -3,7 +3,7 @@
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','https://www.google-analytics.com/analytics_debug.js','ga');
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
 ga('create', 'UA-15705691-1', 'auto');
 /* Enhanced link attribution */
@@ -16,38 +16,11 @@ ga('send', 'pageview');
 $(document).ready(function() {
     var q = 'https://www.lib.uchicago.edu/cgi-bin/subnetclass?jsoncallback=?';
     $.getJSON(q, function(data) {
-        /* Store subnetclass. */
+        // Store subnetclass.
         ga('set', 'dimension1', data);
     });
-
-    // Homepage widget. 
-    
-    // Track outbound links. 
-    $('a[href^="http"]').click(function(e) {
-        var link = $(this).attr('href');
-        ga('send', 'event', 'outbound', 'click', link, {
-            'transport': 'beacon',
-            'hitCallback': function() { document.location = url; }
-        });
-    });
-    
-    // Top bar.
-    // #navbar-right
-    // Deal with the building pulldown. 
-    // Otherwise, eventCategory=topNav, eventAction=click, eventLabel= (the text of the link, but be sure to ignore the building pulldown.) 
-    
-    // #navbar-right #buildingHoursDropdown
-    
-    // TopNav links- everything but the building hours pulldown. 
-    $('#navbar-right a').not('li.dropdown a').click(function(e) {
-        var linktext = $(e.target).text().trim();
-        ga('send', 'event', {
-            eventCategory: 'topNavLink',
-            eventAction: 'click',
-            eventLabel: linktext,
-            transport: 'beacon'
-        });
-    });
+    //$('body').track({ debug: true });
+    $('body').track();
 });
     
     
--- base/static/base/js/jquery.track-everything.js
+++ base/static/base/js/jquery.track-everything.js
@@ -2,163 +2,131 @@
 	$.fn.track = function(options) {
 		// Set defaults
 		var defaults = {
-			additional: [],
-			dictionary: [],
-			options: {
-				forms: true,
-				outbound: true,
-				email: true,
-				phone: true,
-				anchor: true,
-				universal: false,
-				debug: false
-			}
+			outbound: true,
+			debug: false
 		};
-		var settings = $.extend(true, {}, defaults, options );
+		var settings = $.extend(defaults, options);
 
 		$.expr[':'].external = function(obj){
 			return !obj.href.match(/^mailto:/) && !obj.href.match(/^tel:/) && !obj.href.match(/^#/) && (obj.hostname.replace(/^www\./i, '') != document.location.hostname.replace(/^www\./i, ''));
 		};
 
-		var calculateLabel = function ($obj, possibilities){
-			var possibilities = (typeof possibilities == 'object') ? possibilities : ["data-track-everything-name", "name", "title", "id"];
-			var eventLabel = null;
-			for (var i = 0; i <= (possibilities.length - 1); i++) {
-				var possibility = $obj.attr(possibilities[i]);
-				if(possibility && possibility.length){
-					eventLabel = possibility;
-					break;
+		pushEvent = function (params) {
+			var gauEventInfo = {
+				'hitType'      : 'event',
+				'eventCategory': params['category'],
+				'eventAction'  : params['action']
+			};
+			if (params['label']) {
+				gauEventInfo['eventLabel'] = params['label'];
+			}
+			if (params['value']) {
+				gauEventInfo['eventValue'] = params['value'];
+			}
+			if (params['nonInteraction']) {
+				gauEventInfo['nonInteraction'] = Number(params['nonInteraction']);
+			}
+
+			try {
+			    ga('send', gauEventInfo);
+                if (settings.debug) {
+                    console.log(gauEventInfo);
+                } 
+			} catch (e) {
+				if (settings.debug) {
+					console.log("Google Analytics must be installed and initiated for Track Everything to work");
 				}
 			}
-			return eventLabel;
-		},
+		}
 
-		pushEvent = function ( eventInfo ) {
-			if(settings.options.debug){ console.log(eventInfo); }
-			if(settings.options.universal){
-				var gauEventInfo = {
-					'hitType': 'event',
-					'eventCategory': eventInfo[1],
-					'eventAction': eventInfo[2]
-				};
-				if(eventInfo[3]){
-					gauEventInfo['eventLabel'] = eventInfo[3];
-				}
-				if(eventInfo[4]){
-					gauEventInfo['eventValue'] = eventInfo[4];
-				}
-				if(eventInfo[5]){
-					gauEventInfo['nonInteraction'] = Number(eventInfo[4]);
-				}
 
-				try {
-					ga('send', gauEventInfo);
-				} catch (e) {
-					if(settings.options.debug){
-						console.log("Google Analytics must be installed and initiated for Track Everything to work");
-					}
-				}
-
-			}else{
-
-				try {
-					_gaq.push(eventInfo);
-				} catch (e) {
-					if(settings.options.debug){
-						console.log("Google Analytics must be installed and initiated for Track Everything to work");
-					}
-				}
-
-			}
-		},
-
-		trackSpecialLink = function (context, identifier, name) {
-			var $special = $(context).find('a[href^="' + identifier + '"]');
-			$special.on("click.track-everything.track-everything-default keypress.track-everything.track-everything-default", function (e) {
-				var eventLabel = calculateLabel($(this), ["data-track-everything-name", "href"]);
-
-				var elementHref = $(this).attr("href");
-
-				if(eventLabel == elementHref){
-					eventLabel = elementHref.substring(identifier.length);
-				}
-
-				var eventInfo = ['_trackEvent', 'Link', name , eventLabel];
-				pushEvent(eventInfo);
-			});
-			if(settings.options.debug){
-				$special.addClass("track-everything track-everything-default track-everything-" + name.toLowerCase());
-			}
-		};
-
+        // $('body').track() means that this loop will be executed once. 
 		return this.each(function() {
-			for (var i = settings.dictionary.length - 1; i >= 0; i--) {
-				$(this).find(settings.dictionary[i].selector).attr("data-track-everything-name", settings.dictionary[i].name);
-			}
+            $('*[data-ga-category][data-ga-action][data-ga-label]').each(function() {
+                // Skip anything that has been tracked already.
+                if ($(this).is('[data-ga-tracked]')) {
+                    return;
+                }
+                // <a> elements. 
+                if ($(this).is('a')) {
+                    $(this).attr("data-ga-tracked", "on");
+				    $(this).on("click.track-everything keypress.track-everything", function (e) {
+					    pushEvent({
+                            'category': $(this).attr('data-ga-category'),
+                            'action'  : $(this).attr('data-ga-action'),
+                            'label'   : $(this).attr('data-ga-label')
+                        });
+				    });
+                    if (settings.debug) {
+                        console.log('Added tracking for:');
+                        console.log($(this));
+                    }
+                }
+                // <form> elements.
+                if ($(this).is("form")) {
+                    $(this).on("submit.track-everything", function (e) {
+					    pushEvent({
+                            'category': $(this).attr('data-ga-category'),
+                            'action'  : $(this).attr('data-ga-action'),
+                            'label'   : $(this).attr('data-ga-label')
+				        });
+                    });
+                    if (settings.debug) {
+                        console.log('Added tracking for:');
+                        console.log($(this));
+                    }
+                }
+            });
 
-			if(settings.options.forms){
-				var $forms = $(this).find("form");
-				$forms.on("submit.track-everything.track-everything-default", function (e) {
-					var eventLabel = calculateLabel($(this));
+            // Outbound links.
+			if (settings.outbound) {
+                $(this).find("a:external").each(function() {
+                    // Skip anything that has been tracked already.
+                    if ($(this).is('[data-ga-tracked]')) {
+                        return;
+                    }
+                    $(this).attr("data-ga-tracked", "on");
+				    $(this).on("click.track-everything keypress.track-everything", function (e) {
+					    pushEvent({
+                            'category': $(this).attr('data-ga-category'),
+                            'action'  : $(this).attr('data-ga-action'),
+                            'label'   : $(this).attr('data-ga-label')
+                        });
+				    });
+                    if (settings.debug) {
+                        console.log('Added tracking for:');
+                        console.log($(this));
+                    }
+                });
+            }
 
-					var eventInfo = ['_trackEvent', 'Form', 'Submission'];
-					if(eventLabel !== null){
-						eventInfo.push(eventLabel);
-					}
-					pushEvent(eventInfo);
-				});
-				if(settings.options.debug){
-					$forms.addClass("track-everything track-everything-default track-everything-form");
-				}
-			}
-
-			if(settings.options.outbound){
-				var $outbound = $(this).find("a:external");
-				$outbound.on("click.track-everything.track-everything-default keypress.track-everything.track-everything-default", function (e) {
-					var eventLabel = calculateLabel($(this), ["data-track-everything-name", "href"]);
-
-					var eventInfo = ['_trackEvent', 'Link', 'Outbound', eventLabel, null, true];
-					pushEvent(eventInfo);
-				});
-				if(settings.options.debug){
-					$outbound.addClass("track-everything track-everything-default track-everything-outbound");
-				}
-			}
-
-			if(settings.options.email){
-				trackSpecialLink(this, "mailto:", "Email");
-			}
-
-			if(settings.options.phone){
-				trackSpecialLink(this, "tel:", "Phone");
-			}
-
-			if(settings.options.anchor){
-				trackSpecialLink(this, "#", "Anchor");
-			}
-
-			var context = this;
-
-			$.each(settings.additional, function (i, additional){
-				var $additional = $(context).find(additional.selector);
-				$additional.off(".track-everything-default");
-				if(settings.options.debug){
-					$additional.addClass("track-everything track-everything-additional").removeClass("track-everything-default");
-				}
-				var events = [];
-				for (var j = additional.events.length - 1; j >= 0; j--) {
-					events.push(additional.events[j] + ".track-everything.track-everything-additional");
-				}
-				$additional.on(events.join(" "), function () {
-					if(additional.name.length){
-						$(this).attr("data-track-everything-override", additional.name);
-					}
-					var eventLabel = calculateLabel($(this), ["data-track-everything-override", "data-track-everything-name", "name", "title", "id", "href"]);
-					var eventInfo = ['_trackEvent', additional.category, additional.action, eventLabel];
-					
-					pushEvent(eventInfo);
-				});
-			});
+            /* For debugging, check to see if anything with a data-ga-category, data-ga-action,
+             * or data-ga-label wasn't tracked. 
+             */
+            if (settings.debug) {
+                $('body').find('[data-ga-category]:not([data-ga-tracked])').each(function () {
+                    console.log('Element has a data-ga-category attribute, but was not tracked.');
+                    console.log($(this));
+                });
+                $('body').find('[data-ga-action]:not([data-ga-tracked])').each(function () {
+                    console.log('Element has a data-ga-action attribute, but was not tracked.');
+                    console.log($(this));
+                });
+                $('body').find('[data-ga-label]:not([data-ga-tracked])').each(function () {
+                    console.log('Element has a data-ga-label attribute, but was not tracked.');
+                    console.log($(this));
+                });
+            }
 		});
 	};
 }( jQuery ));
+
+    /* Track outbound links. 
+    $('a[href^="http"]').click(function(e) {
+        var link = $(this).attr('href');
+        ga('send', 'event', 'outbound', 'click', link, {
+            'transport': 'beacon',
+            'hitCallback': function() { document.location = url; }
+        });
+    });
+    */
