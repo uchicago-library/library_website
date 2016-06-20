23174ecfb6f7bbd931854b72098f1d44770ad858
John Jung <jej@moss.lib.uchicago.edu>
2016-06-20 13:44:43 -0500

    Added fields to search widget pulldown.

--- base/static/base/js/library_website.js
+++ base/static/base/js/library_website.js
@@ -71,6 +71,32 @@ $(document).ready(function(){
     });
 
     /*
+     * Search Widget, Catalog search, Begins-With searches.
+     */
+
+    // If this script is running (i.e., JavaScript is enabled) add dropdown elements for browses. 
+    $('#search_widget_catalog_search').each(function() {
+        var select = $(this).find('select[name="type"]');
+        select.append('<option value="browse_title">Title begins with</option><option value="browse_journal">Journal begins with</option><option value="browse_lcc">Call Number begins with</option>');
+    });
+
+    // Modify browses before the form is submitted.
+    $('#search_widget_catalog_search').submit(function() {
+        // If the field pulldown's value begins with "browse_"...
+        var type = $(this).find('select[name="type"]').val();
+        if (type.substring(0, 7) == 'browse_') {
+            // Make a new hidden element called "source" for everything after "browse_". 
+            $('#search_widget_catalog_search').append('<input name="source" type="hidden" value="' + type.substring(7) + '"/>');
+            // Remove the original "type" element.
+            $(this).find('select[name="type"]').remove();
+            // Change the name of the text input from "lookfor" to "from". 
+            $(this).find('input[name="lookfor"]').attr('name', 'from');
+            // Change the action of the form so it points to the browse result pages. 
+            $(this).attr('action', 'https://catalog.lib.uchicago.edu/vufind/Alphabrowse/Home');
+        }
+    });
+
+    /*
      * Lightbox
      */
     $(document).delegate('*[data-toggle="lightbox"]', 'click', function(event) {
