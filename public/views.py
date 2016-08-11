2be8bb4bdecf3faeb8a8c0188efb312591fa36d8
Jose Arreluce <josearreluce@UCHICAGO.EDU>
2016-08-11 12:56:38 -0500

    Retitled Search tab and removed buildings filtering

--- public/views.py
+++ public/views.py
@@ -33,8 +33,6 @@ def spaces(request):
 
     # get spaces.
     spaces = LocationPage.objects.live()
-    if building:
-        spaces = spaces.filter(parent_building = LocationPage.objects.get(title=building))
     if feature:
         spaces = spaces.filter(**{feature: True})
     if floor:
