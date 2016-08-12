ff373c2596ba761549fd38ce8d83f3cb848ee5b0
Jose Arreluce <josearreluce@UCHICAGO.EDU>
2016-08-12 10:28:36 -0500

    Fixed 'Browse All Subjects' link in Units app/page

--- public/views.py
+++ public/views.py
@@ -34,7 +34,7 @@ def spaces(request):
     # get spaces.
     spaces = LocationPage.objects.live()
     if building:
-	    spaces = spaces.filter(parent_building = LocationPage.objects.get(title=building))
+        spaces = spaces.filter(parent_building = LocationPage.objects.get(title=building))
     if feature:
         spaces = spaces.filter(**{feature: True})
     if floor:
