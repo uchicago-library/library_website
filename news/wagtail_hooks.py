d95e8880c1efb5c2a8037412ed9d73a966cbc147
John Jung <jej@motacilla.lib.uchicago.edu>
2016-09-12 09:52:08 -0500

    Debugging for Loop news notifications test code.

--- news/wagtail_hooks.py
+++ news/wagtail_hooks.py
@@ -12,10 +12,15 @@ def admin_view(request):
     if request.method == 'POST':
         form = EmailNotificationTestForm(request.POST)
         if form.is_valid():
-            from_email = form.data.get('from_email', None)
-            to_email = form.data.get('to_email', None)
+            from_email = form.data.get('email_from', None)
+            to_email = form.data.get('email_to', None)
             num_days = form.data.get('num_days', None)
-            end_date = form.data.get('end_date', None)
+            if num_days:
+                num_days = int(num_days)
+            # strip dashes out of the date, so YYYY-MM-DD becomes YYYYMMDD
+            end_date = form.data.get('email_as_if_date', None)
+            if end_date:
+                end_date = end_date.replace("-", "")
            
             if from_email != None and to_email != None and num_days != None and end_date != None: 
                 send_loop_news_email_notifications(from_email, to_email, num_days, end_date)
