402052fce2eae823c81a30233c72220e6f19b24b
John Jung <jej@motacilla.lib.uchicago.edu>
2017-02-10 10:22:43 -0600

    Added a shell script for reporting weekly news stories.

---
+++ bin/report_daily_news_stories.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+/data/motacilla/venv3.4/bin/activate && /data/motacilla/sites/library_website/manage.py report_daily_news_stories eee@uchicago.edu lib-staff@lib.uchicago.edu 7 `date -v -1d "+%Y%m%d"`
+
+
+
