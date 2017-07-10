43a1bdea8dd69f70abf09f7f3dd6625a65396267
John Jung <jej@moss.lib.uchicago.edu>
2017-07-10 11:16:44 -0500

    Added a management command for keeping 2017 updates to staff pages in sync.

--- staff/utils.py
+++ staff/utils.py
@@ -134,6 +134,123 @@ def get_individual_info_from_directory(xml_string):
         info['title_department_subdepartments'].add("\n".join(output))
         info['title_department_subdepartments_dicts'].append(output_dict)
 
+    info['phoneFacultyExchanges'] = set()
+    info['phoneFacultyExchanges_dicts'] = []
+    for vcard in x.findall("individuals/individual/contacts/contact"):
+        try:
+            if vcard.find('division/name').text != 'Library':
+                continue
+        except:
+            continue
+   
+        output = [] 
+        output_dict = {}
+
+        try:
+            facultyexchange = re.sub('\s+', ' ', vcard.find('facultyExchange').text).strip()
+            if facultyexchange:
+                output.append(facultyexchange)
+                output_dict['facultyexchange'] = facultyexchange
+        except:
+            pass
+
+        try:
+            phone = re.sub('\s+', ' ', vcard.find('phone').text).strip()
+            if phone:
+                chunks = re.search('^\(([0-9]{3})\) ([0-9]{3})-([0-9]{4})$', phone)
+                phone_number = chunks.group(1) + "-" + chunks.group(2) + "-" + chunks.group(3)
+                output.append(phone_number)
+                output_dict['phone'] = phone_number
+        except:
+            pass
+
+        info['phoneFacultyExchanges'].add("\n".join(output))
+        info['phoneFacultyExchanges_dicts'].append(output_dict)
+
+    return info
+
+def get_individual_info_from_directory_2017(xml_string):
+    x = ElementTree.fromstring(xml_string)
+
+    info = {}
+
+    info['cnetid'] = x.find("individuals/individual/cnetid").text
+
+    # name is slightly more formal- e.g. "John E. Jung"
+    info['officialName'] = x.find("individuals/individual/name").text
+
+    # displayName is a bit more casual- e.g. "John Jung"
+    info['displayName'] = x.find("individuals/individual/displayName").text
+
+    #  title, department, subdepartment, building/room number, phone number:
+    #  many individuals have more than one title- for example, 
+    #  Emily Treptow has the following two strings as titles:
+    #  "Business and Economics Librarian for Instruction & Outreach"
+    #  "Business & Economics Librarian for Instruction & Outreach"
+    #  Show both titles, even though they're close, to encourage
+    #  people to fix them in the university's system. 
+    #
+    #  The title doesn't make sense without a department, subdepartment
+    #  pair though. And many people, especially bibliographers, 
+    #  have three or four title/department/subdepartments. 
+    #
+    # Also, some titles have different rooms- see Laura Ring for 
+    # an example. Many of those people, like Paul Belloni, will 
+    # have multiple phone numbers. (It looks like Paul has a phone
+    # number at the SSA and at the Reg.)
+    #
+    # Each title gets it's own table? Each one points at a department/
+    # subdepartment pair? (in it's own table...)
+
+    # first title only. 
+    info['position_title'] = re.sub('\s+', ' ', x.find("individuals/individual/contacts/contact/title").text).strip()
+
+    info['email'] = set()
+    for vcard in x.findall("individuals/individual/contacts/contact"):
+        try:
+            if vcard.find('division/name').text != 'Library':
+                continue
+        except:
+            continue
+        try:
+            info['email'].add(re.sub('\s+', ' ', vcard.find('email').text).strip())
+        except:
+            pass
+
+    info['departments'] = set()
+
+    info['phone_facultyexchanges'] = set()
+    for vcard in x.findall("individuals/individual/contacts/contact"):
+        try:
+            if vcard.find('division/name').text != 'Library':
+                continue
+        except:
+            continue
+   
+        output = [] 
+        output_dict = {}
+
+        try:
+            facultyexchange = re.sub('\s+', ' ', vcard.find('facultyExchange').text).strip()
+            if facultyexchange:
+                output.append(facultyexchange)
+                output_dict['facultyexchange'] = facultyexchange
+        except:
+            pass
+
+        try:
+            phone = re.sub('\s+', ' ', vcard.find('phone').text).strip()
+            if phone:
+                chunks = re.search('^\(([0-9]{3})\) ([0-9]{3})-([0-9]{4})$', phone)
+                phone_number = chunks.group(1) + "-" + chunks.group(2) + "-" + chunks.group(3)
+                output.append(phone_number)
+                output_dict['phone'] = phone_number
+        except:
+            pass
+
+        info['title_department_subdepartments'].add("\n".join(output))
+        info['title_department_subdepartments_dicts'].append(output_dict)
+
     return info
 
 def get_all_library_cnetids_from_wagtail():
@@ -186,5 +303,52 @@ def get_individual_info_from_wagtail(cnetid):
         output['title_department_subdepartments'].add("\n".join(tmp))
 
     return output
+
+# the data format changed with our 2017 update to the StaffPage object.
+# instead of keeping VCards in bundles, now they are split into different
+# pieces. 
+#
+# email addresses are repeatable but not bundled. units are repeatable
+# but not bundled. phone numbers and faculty exchange are bundled together,
+# and repeatable. 
+#
+# TODO: Add units. 
+# 
+def get_individual_info_from_wagtail_2017(cnetid):
+    staff_page = StaffPage.objects.get(cnetid=cnetid)
+
+    if staff_page.display_name == None:
+        raise ValueError(cnetid + ' has a display_name of None.')
+
+    if staff_page.official_name == None:
+        raise ValueError(cnetid + ' has an official_name of None.')
+       
+    # officialName is slightly more formal- e.g. "John E. Jung"
+    # displayName is a bit more casual- e.g. "John Jung"
+    output = {
+        "cnetid": cnetid,
+        "officialName": staff_page.official_name,
+        "displayName": staff_page.display_name,
+        "positionNitle": staff_page.position_title,
+        "email": set(staff_page.staff_page_email.all().values_list('email', flat=True)),
+        "departments": set(),
+        "phoneFacultyExchanges": set()
+    }
+
+    for v in staff_page.staff_page_phone_faculty_exchange.all():
+        tmp = []
+
+        faculty_exchange = v.faculty_exchange
+        if faculty_exchange:
+            tmp.append(re.sub('\s+', ' ', faculty_exchange).strip())
+
+        phone_number = v.phone_number
+        if phone_number:
+            tmp.append(re.sub('\s+', ' ', phone_number).strip())
+
+        output['phoneFacultyExchanges'].add("\n".join(tmp))
+
+    return output
+
 
 
