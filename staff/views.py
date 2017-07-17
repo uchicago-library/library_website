a68c09b72289925fc3947f6880f659b8eaeb79cc
John Jung <jej@moss.lib.uchicago.edu>
2017-07-17 11:05:24 -0500

    Removed unused imports for DirectoryUnit and StaffPageVCards.

--- staff/views.py
+++ staff/views.py
@@ -1,10 +1,10 @@
-from directory_unit.models import DirectoryUnit
 from django.db.models import Q
 from django.shortcuts import render
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from intranetunits.models import IntranetUnitsIndexPage, IntranetUnitsPage
 from subjects.models import Subject
-from staff.models import StaffPage, StaffPagePageVCards, StaffPageSubjectPlacement
+from staff.models import StaffPage, StaffPageSubjectPlacement
+from units.views import get_staff_pages_for_library
 
 from wagtail.wagtailcore.models import Page
 from wagtail.wagtailimages.models import Image
@@ -23,37 +23,8 @@ def staff(request):
     flat_units = []
 
     if view == 'staff':
-        staff_pks = []
         if library:
-            # get a queryset of units from each library building, then get a list of distinct StaffPage pks for each queryset. 
-            if library == 'Eckhart Library':
-                eckhart_units = DirectoryUnit.objects.get(name='Eckhart Library').get_descendants(True)
-                staff_pks = StaffPagePageVCards.objects.filter(unit__in=eckhart_units).values_list('page', flat=True).distinct()
-            elif library == 'Crerar Library':
-                crerar_units = DirectoryUnit.objects.get(name='Science Libraries').get_descendants(True)
-                eckhart_units = DirectoryUnit.objects.get(name='Eckhart Library').get_descendants(True)
-                staff_pks = StaffPagePageVCards.objects.filter(unit__in=crerar_units).exclude(unit__in=eckhart_units).values_list('page', flat=True).distinct()
-            elif library == 'D\'Angelo Law Library':
-                dangelo_units = DirectoryUnit.objects.get(name='D\'Angelo Law Library').get_descendants(True)
-                staff_pks = StaffPagePageVCards.objects.filter(unit__in=dangelo_units).values_list('page', flat=True).distinct()
-            elif library == 'SSA Library':
-                ssa = DirectoryUnit.objects.get(name='Social Service Administration Library (SSA)').get_descendants(True)
-                staff_pks = StaffPagePageVCards.objects.filter(unit__in=ssa).values_list('page', flat=True).distinct()
-            elif library == 'Regenstein Library':
-                crerar = DirectoryUnit.objects.get(name='Science Libraries').get_descendants(True)
-                dangelo = DirectoryUnit.objects.get(name='D\'Angelo Law Library').get_descendants(True)
-                ssa = DirectoryUnit.objects.get(name='Social Service Administration Library (SSA)').get_descendants(True)
-                staff_pks = StaffPagePageVCards.objects.all().exclude(unit__in=crerar).exclude(unit__in=dangelo).exclude(unit__in=ssa).values_list('page', flat=True).distinct()
-            elif library == 'Special Collections Research Center':
-                scrc = DirectoryUnit.objects.get(name='Special Collections Research Center').get_descendants(True)
-                staff_pks = StaffPagePageVCards.objects.all().filter(unit__in=scrc).values_list('page', flat=True).distinct()
-            elif library == 'Mansueto':
-                mansueto = DirectoryUnit.objects.filter(Q(name='Mansueto') | Q(name='Mansueto Library'))
-                staff_pks = StaffPagePageVCards.objects.all().filter(unit__in=mansueto).values_list('page', flat=True).distinct()
-                
-        # get StaffPages themselves from the pk list. 
-        if staff_pks:
-            staff_pages_all = StaffPage.objects.filter(pk__in=staff_pks).order_by('last_name', 'first_name')
+            staff_pages_all = get_staff_pages_for_library(library)
         else:
             staff_pages_all = StaffPage.objects.live().order_by('last_name', 'first_name')
     
