64d1dee93fd844418c52a4494603b9b690d5802a
John Jung <jej@moss.lib.uchicago.edu>
2017-09-13 15:01:53 -0500

    Added extra text to the unit reporting form.

--- units/forms.py
+++ units/forms.py
@@ -19,5 +19,5 @@ class UnitReportingForm(forms.Form):
     display_in_campus_directory = forms.BooleanField(label=l, required=False)
 
     v = RegexValidator(r'^[0-9]{8}$', 'Please enter dates in YYYYMMDD format.')
-    l = 'Latest revision created at'
+    l = 'Latest revision created at YYYYMMDD'
     latest_revision_created_at = forms.CharField(label=l, required=False, validators=[v], widget=AdminDateInput())
