1a7c2a169366df0035b70e20da2dfa790cc29142
John Jung <jej@moss.lib.uchicago.edu>
2017-09-15 14:45:56 -0500

    HR staff report now launches a subprocess.

--- staff/wagtail_hooks.py
+++ staff/wagtail_hooks.py
@@ -1,18 +1,12 @@
 from django.conf.urls import url
 from django.core import management
 from django.core.urlresolvers import reverse
-from django.http import HttpResponse
 from django.shortcuts import render
-from email.mime.application import MIMEApplication
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
-from email.utils import formatdate
-from openpyxl.writer.excel import save_virtual_workbook
-from staff.utils import WagtailStaffReport
 from wagtail.wagtailadmin.menu import MenuItem
 from wagtail.wagtailcore import hooks
 
-import smtplib
+import os
+import subprocess
 
 def admin_view(request):
     # Needs to be inside for unit tests
@@ -42,30 +36,28 @@ def admin_view(request):
                 continue
 
         if form.is_valid():
-            staff_report = WagtailStaffReport(
-                sync_report = True,
-                staff_report = True,
-                all = not options['live'],
-                **options
+            manage_dir = os.path.dirname(
+                os.path.dirname(os.path.abspath(__file__))
             )
-
-            virtual_workbook = save_virtual_workbook(staff_report.workbook())
-
-            msg = MIMEMultipart()
-            msg['From'] = 'jej@uchicago.edu'
-            msg['To'] = options['email_to']
-            msg['Date'] = formatdate(localtime=True)
-            msg['Subject'] = 'Library Staff Report'
-
-            msg.attach(MIMEText('A report of Library staff is attached.'))
-            attachment = MIMEApplication(virtual_workbook, name=options['filename'] + '.xlsx')
-            attachment['Content-Disposition'] = 'attachment; filename="{}.xlsx"'.format(options['filename'])
-            msg.attach(attachment)
-
-            s = smtplib.SMTP('localhost')
-            s.send_message(msg)
-            s.quit()
-
+            args = [
+                'python',
+                manage_dir + os.path.sep + 'manage.py',
+                'list_staff_wagtail',
+                '--report-out-of-sync-staff',
+                '--output-format=excel'
+            ]
+            for k, v in options.items():
+                if bool(v):
+                    if k == 'filename':
+                        args.append('--{}={}.xlsx'.format(k, v))
+                    elif k in ('all',
+                               'live',
+                               'supervises-students',
+                               'supervisor-override'):
+                        args.append('--{}'.format(k))
+                    else:
+                        args.append('--{}={}'.format(k, v))
+            subprocess.Popen(args)
             return render(request, 'staff/staff_reporting_form_thank_you.html') 
         else:
             return render(request, 'staff/staff_reporting_form.html', {'form': form})
