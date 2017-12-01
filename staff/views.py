e67c3377181c96616b11e8a9b8bbf32c343aa181
John Jung <jej@moss.lib.uchicago.edu>
2017-12-01 14:26:42 -0600

    Fixed bug involving SSA pulldown on Loop staff listing page.

--- staff/views.py
+++ staff/views.py
@@ -2,10 +2,10 @@ from django.db.models import Q
 from django.shortcuts import render
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from intranetunits.models import IntranetUnitsIndexPage, IntranetUnitsPage
+from public.models import LocationPage
 from subjects.models import Subject
 from staff.models import StaffPage, StaffPageSubjectPlacement
 from units.views import get_staff_pages_for_library
-
 from wagtail.wagtailcore.models import Page
 from wagtail.wagtailimages.models import Image
 from wagtail.wagtailsearch.models import Query
@@ -83,7 +83,7 @@ def staff(request):
     return render(request, 'staff/staff_index_page.html', {
         'default_image': default_image,
         'flat_intranet_units': flat_units,
-        'libraries': ["Regenstein Library", "Crerar Library", "D'Angelo Law Library", "Eckhart Library", "Mansueto", "Special Collections Research Center", "SSA Library"],
+        'libraries': [str(p) for p in LocationPage.objects.live().filter(is_building=True)],
         'library': library,
         'query': query,
         'subject': subject,
