79c1c691bb879442c584040588e796834cceb93a
Kathy Zadrozny <kzadrozny@atlantis.lib.uchicago.edu>
2018-02-08 16:36:00 -0600

    Typography and Search Styling Updates
    Includes new header colors and staff page layout.

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,34 @@
-"use strict";
+'use strict';
+ 
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+ 
+gulp.task('sass', function () {
+  return gulp.src('base/static/base/css/loop/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('base/static/base/css/'));
+});
+ 
+gulp.task('sass:watch', function () {
+  gulp.watch('base/static/base/css/loop/*.scss', ['sass']);
+});
+
+gulp.task("default", ["sass"], function() {
+});
 
 var gulp = require('gulp');
 var sass = require('gulp-sass'); // Requires the gulp-sass plugin
 
 
 gulp.task('sass', function(){
-  return gulp.src('base/static/base/css/loop-styles.scss')
+  return gulp.src('base/static/base/css/loop/*.scss') // Gets all files ending with .scss in src/scss and children dirs
     .pipe(sass()) // Using gulp-sass
     .pipe(gulp.dest('base/static/base/css'))
 });
 
+gulp.task('watch', ['sass'], function(){
+  gulp.watch('base/static/base/css/loop/*.scss', ['sass']);
+})
 
-gulp.task("default", ["sass"], function() {
+gulp.task("default", ["sass", "watch"], function() {
 });
