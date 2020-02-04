9ad677bdf15749235c702a4f0049e2a6cab9c05a
Matt Teichman <teichman@uchicago.edu>
2020-02-04 11:22:22 -0600

    run auto-fixers on lib_news/views.py and also replace placeholder RSS feed description with something less informal in tone

--- lib_news/views.py
+++ lib_news/views.py
@@ -1,12 +1,15 @@
-from requests import get
-from django.contrib.syndication.views import Feed
-from lib_news.models import LibNewsPage, LibNewsIndexPage, LibNewsPageCategories, PublicNewsCategories
-from django.http.response import StreamingHttpResponse
-from wagtailcache.cache import cache_page
-from django.utils.decorators import method_decorator
 from datetime import date, timedelta
-from django.utils.text import slugify
 
+from django.contrib.syndication.views import Feed
+from django.http.response import StreamingHttpResponse
+from django.utils.decorators import method_decorator
+from django.utils.text import slugify
+from requests import get
+from wagtailcache.cache import cache_page
+
+from lib_news.models import (
+    LibNewsIndexPage, LibNewsPage, LibNewsPageCategories, PublicNewsCategories
+)
 from library_website.settings import STATIC_NEWS_FEED
 
 
@@ -20,6 +23,7 @@ def ltdrfr(request):
 
 
 class RSSFeeds(Feed):
+
     def get_object(self, request, slug):
         """Part of the Django syndication API; in this case, returns
         the category of the feed.
@@ -29,7 +33,7 @@ class RSSFeeds(Feed):
             return PublicNewsCategories.objects.filter(text=category).first()
         except KeyError:
             return None
-            
+
     def title(self, obj):
         """Title for the whole feed.
         """
@@ -37,29 +41,29 @@ class RSSFeeds(Feed):
             return "RSS Feed for the %s News Category" % obj.text
         else:
             return "RSS Feed for Library News"
-    
+
     link = "/rss/"
-    
-    description = 'News Stories, UChicago Library!'
-    
+
+    description = 'News Stories: The University of Chicago Library'
+
     def items(self, obj):
         """Generates the list of items in the feed."""
+
         def has_category(cat):
+
             def partial_application(page):
                 return cat in page.get_categories()
+
             return partial_application
 
-        stories = (LibNewsPage
-                   .objects
-                   .order_by('-published_at')
-        )
+        stories = LibNewsPage.objects.order_by('-published_at')
 
         if obj:
             c = obj.text
             return list(filter(has_category(c), stories))[:20]
         else:
             return stories[:20]
-    
+
     def item_title(self, item):
         """Title for each feed story."""
         return item.title
@@ -71,7 +75,7 @@ class RSSFeeds(Feed):
     def item_pubdate(self, item):
         """Publication date of each story."""
         return item.published_at
-    
+
     def item_link(self, item):
         """Link to each story."""
         return item.url
