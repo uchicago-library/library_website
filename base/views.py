e3ab7ed1b78275d39dbffe5ba7b09acf74115a41
Brad Busenius <bbusenius@users.noreply.github.com>
2020-05-05 13:25:43 -0500

    Fixed deprecations and removed unused code in preparation for the upgrade to Wagtail 2.9.

--- base/views.py
+++ base/views.py
@@ -20,25 +20,6 @@ from public.models import StandardPage
 from units.utils import get_default_unit
 
 
-def breadcrumbs(request):
-    breadcrumbs = [{"href": "/", "text": "Home"}]
-
-    path_components = [
-        component for component in request.path.split('/') if component
-    ]
-    page, args, kwargs = request.site.root_page.specific.route(
-        request, path_components
-    )
-    while page:
-        breadcrumbs.append({"href": page.url, "text": page.title})
-        if hasattr(page, 'parent'):
-            page = page.parent
-        else:
-            break
-
-    return breadcrumbs
-
-
 def json_hours(request):
     """
     View for rendering hours as json.
