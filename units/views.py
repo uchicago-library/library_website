abd3f1923e7e9961bccb878c683915d735cf75f3
Matt Teichman <teichman@uchicago.edu>
2020-07-10 15:57:15 -0500

    remove June merge conflict with units/views.py

--- units/views.py
+++ units/views.py
@@ -1,32 +1,29 @@
-from .models import Tree
-from .forms import UnitReportingForm
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-from django.db.models import Q
-from django.db.models.expressions import RawSQL
+import re
+
 from django.http import HttpResponse
 from django.shortcuts import render
 from openpyxl.writer.excel import save_virtual_workbook
+from wagtail.core.models import Site
+from wagtail.images.models import Image
+
+from alerts.utils import get_browse_alerts
+from ask_a_librarian.utils import (
+    get_chat_status, get_chat_status_css, get_unit_chat_link
+)
+from base.utils import get_hours_and_location
+from library_website.settings import PUBLIC_HOMEPAGE
+from public.models import LocationPage, StandardPage
 from staff.models import StaffPage, StaffPageSubjectPlacement
 from subjects.models import Subject
 from units.models import UnitIndexPage, UnitPage
-from units.utils import get_quick_nums_for_library_or_dept
-from units.utils import WagtailUnitsReport
-from wagtail.images.models import Image
-from public.models import StandardPage, LocationPage
-from library_website.settings import PUBLIC_HOMEPAGE, QUICK_NUMS, REGENSTEIN_HOMEPAGE, SSA_HOMEPAGE, MANSUETO_HOMEPAGE, CRERAR_HOMEPAGE, ECKHART_HOMEPAGE, DANGELO_HOMEPAGE, SCRC_HOMEPAGE
-from base.utils import get_hours_and_location
-from ask_a_librarian.utils import get_chat_status, get_chat_status_css, get_unit_chat_link
-from django.utils.text import slugify
-from wagtailcache.cache import cache_page
+from units.utils import WagtailUnitsReport, get_quick_nums_for_library_or_dept
+
+from .forms import UnitReportingForm
 
 
-import os
-import re
-import subprocess
-import urllib.parse
-
-
-def get_staff_pages_for_unit(unit_page_full_name = None, recursive = False, display_supervisor_first = False):
+def get_staff_pages_for_unit(
+    unit_page_full_name=None, recursive=False, display_supervisor_first=False
+):
     unit_page_ids = None
     unit_page = None
     if unit_page_full_name:
@@ -34,22 +31,32 @@ def get_staff_pages_for_unit(unit_page_full_name = None, recursive = False, disp
             if u.get_full_name() == unit_page_full_name:
                 unit_page = u
                 if recursive:
-                    unit_page_ids = list(u.get_descendants(True).values_list('id', flat=True))
+                    unit_page_ids = list(
+                        u.get_descendants(True).values_list('id', flat=True)
+                    )
                 else:
                     unit_page_ids = [u.id]
                 break
 
     if unit_page_ids == None:
         recursive = True
-        unit_page_ids = list(UnitIndexPage.objects.first().get_descendants(True).values_list('id', flat=True))
- 
-    staff_pages = StaffPage.objects.live().filter(staff_page_units__library_unit__id__in=unit_page_ids).distinct().order_by('last_name', 'first_name')
-    
+        unit_page_ids = list(
+            UnitIndexPage.objects.first().get_descendants(True).values_list(
+                'id', flat=True
+            )
+        )
+
+    staff_pages = StaffPage.objects.live().filter(
+        staff_page_units__library_unit__id__in=unit_page_ids
+    ).distinct().order_by('last_name', 'first_name')
+
     if display_supervisor_first:
         if unit_page and unit_page.department_head and unit_page.department_head.live:
-            staff_pages = [unit_page.department_head] + list(staff_pages.exclude(id=unit_page.department_head.id))
-        
+            staff_pages = [unit_page.department_head] + \
+                list(staff_pages.exclude(id=unit_page.department_head.id))
+
     return staff_pages
+
 
 def get_libraries():
     return sorted(
@@ -58,10 +65,9 @@ def get_libraries():
     )
 
 
-@cache_page
 def units(request):
     divisions = []
-    
+
     department = request.GET.get('department', None)
     library = request.GET.get('library', None)
     page = request.GET.get('page', 1)
@@ -79,8 +85,10 @@ def units(request):
     departments = []
 
     if query:
-      staff_pages = StaffPage.objects.live().search(query)
-      departments = UnitPage.objects.filter(display_in_library_directory=True, live=True).search(query)
+        staff_pages = StaffPage.objects.live().search(query)
+        departments = UnitPage.objects.filter(
+            display_in_library_directory=True, live=True
+        ).search(query)
 
     elif view == 'staff':
         if library == None:
@@ -95,23 +103,42 @@ def units(request):
         # subjects.
         if subject:
             if subject == 'All Subject Specialists':
-                staff_pages = staff_pages.filter(id__in=StaffPageSubjectPlacement.objects.all().values_list('page', flat=True).distinct())
+                staff_pages = staff_pages.filter(
+                    id__in=StaffPageSubjectPlacement.objects.all().
+                    values_list('page', flat=True).distinct()
+                )
             else:
-                # get a subject and all it's descendants. 
-                subject_and_descendants = Subject.objects.get(name=subject).get_descendants()
-                # from staff page subject placements, get all of the staff that match those subjects. 
-                subject_staff_ids = StaffPageSubjectPlacement.objects.filter(subject__in=subject_and_descendants).values_list('page', flat=True)
-                # filter staff_pages to only include those staff pages. 
-                staff_pages = staff_pages.filter(id__in=subject_staff_ids).order_by('last_name')
+                # get a subject and all it's descendants.
+                subject_and_descendants = Subject.objects.get(
+                    name=subject
+                ).get_descendants()
+                # from staff page subject placements, get all of the staff that match those subjects.
+                subject_staff_ids = StaffPageSubjectPlacement.objects.filter(
+                    subject__in=subject_and_descendants
+                ).values_list(
+                    'page', flat=True
+                )
+                # filter staff_pages to only include those staff pages.
+                staff_pages = staff_pages.filter(id__in=subject_staff_ids
+                                                 ).order_by('last_name')
 
     elif view == 'department':
-      divisions = []
-      for division in UnitIndexPage.objects.first().get_children().specific().type(UnitPage).filter(live=True, unitpage__display_in_library_directory=True).order_by('title'):
-        divisions.append({
-          'unit': division,
-          'descendants': division.get_descendants().specific().type(UnitPage).filter(live=True, unitpage__display_in_library_directory=True).order_by('title')
-        })
-    
+        divisions = []
+        for division in UnitIndexPage.objects.first().get_children().specific(
+        ).type(UnitPage).filter(
+            live=True, unitpage__display_in_library_directory=True
+        ).order_by('title'):
+            divisions.append(
+                {
+                    'unit':
+                    division,
+                    'descendants':
+                    division.get_descendants().specific().type(UnitPage).filter(
+                        live=True, unitpage__display_in_library_directory=True
+                    ).order_by('title')
+                }
+            )
+
     default_image = Image.objects.get(title="Default Placeholder Photo")
 
     try:
@@ -124,46 +151,63 @@ def units(request):
     location_and_hours = get_hours_and_location(home_page)
     location = str(location_and_hours['page_location'])
     unit = location_and_hours['page_unit']
+    current_site = Site.find_for_request(request)
+    alert_data = get_browse_alerts(current_site)
 
     title = ''
     if query:
-      title = 'Search Results'
+        title = 'Search Results'
     elif view == 'staff':
-      title = 'Library Directory: Staff'
-    elif view == 'department': 
-      title = 'Library Directory: Departments'
+        title = 'Library Directory: Staff'
+    elif view == 'department':
+        title = 'Library Directory: Departments'
     elif view == 'org':
-      title = 'Library Directory: Org Chart'
+        title = 'Library Directory: Org Chart'
 
-    quick_nums = get_quick_nums_for_library_or_dept(request).replace('<td>', '<li>').replace('</td>', '</li>')
+    quick_nums = get_quick_nums_for_library_or_dept(request).replace(
+        '<td>', '<li>'
+    ).replace('</td>', '</li>')
 
-    return render(request, 'units/unit_index_page.html', {
-        'breadcrumb_div_css': 'col-md-12 breadcrumbs hidden-xs hidden-sm',
-        'content_div_css': 'container body-container directory col-xs-12 col-lg-11 col-lg-offset-1',
-        'department': department,
-        'departments': departments,
-        'default_image': default_image,
-        'divisions': divisions,
-        'libraries': get_libraries(),
-        'library': library,
-        'org_chart_image': org_chart_image,
-        'query': query,
-        'staff_pages': staff_pages,
-        'subjects': Subject.objects.filter(display_in_dropdown=True).values_list('name', flat=True),
-        'subject': subject,
-        'view': view,
-        'self': {
-            'title': title
-        },
-        'page_unit': str(unit),
-        'page_location': location,
-        'address': location_and_hours['address'],
-        'chat_url': get_unit_chat_link(unit, request),
-        'chat_status': get_chat_status('uofc-ask'),
-        'chat_status_css': get_chat_status_css('uofc-ask'),
-        'hours_page_url': home_page.get_hours_page(request),
-        'quick_nums': quick_nums
-    })
+    subjects = Subject.objects.filter(display_in_dropdown=True).values_list(
+        'name', flat=True
+    ),
+
+    return render(
+        request, 'units/unit_index_page.html', {
+            'breadcrumb_div_css': 'col-md-12 breadcrumbs hidden-xs hidden-sm',
+            'content_div_css':
+            'container body-container directory col-xs-12 col-lg-11 col-lg-offset-1',
+            'department': department,
+            'departments': departments,
+            'default_image': default_image,
+            'divisions': divisions,
+            'libraries': get_libraries(),
+            'library': library,
+            'org_chart_image': org_chart_image,
+            'query': query,
+            'staff_pages': staff_pages,
+            'subjects': subjects[0],
+            'subject': subject,
+            'view': view,
+            'self': {
+                'title': title
+            },
+            'page_unit': str(unit),
+            'page_location': location,
+            'address': location_and_hours['address'],
+            'chat_url': get_unit_chat_link(unit, request),
+            'chat_status': get_chat_status('uofc-ask'),
+            'chat_status_css': get_chat_status_css('uofc-ask'),
+            'hours_page_url': home_page.get_hours_page(request),
+            'quick_nums': quick_nums,
+            'has_alert': alert_data[0],
+            'alert_message': alert_data[1][0],
+            'alert_level': alert_data[1][1],
+            'alert_more_info': alert_data[1][2],
+            'alert_link': alert_data[1][3],
+        }
+    )
+
 
 def unit_reporting_admin_view(request):
     """
@@ -172,28 +216,35 @@ def unit_reporting_admin_view(request):
     if request.method == 'POST':
         form = UnitReportingForm(request.POST)
         options = {
-            'display_in_campus_directory': form.data.get('display_in_campus_directory', False),
-            'email_to': form.data.get('email_to', None),
-            'filename': form.data.get('filename', 'unit_report'),
-            'live': form.data.get('live', None),
-            'latest_revision_created_at': form.data.get('latest_revision_created_at', None),
+            'display_in_campus_directory':
+            form.data.get('display_in_campus_directory', False),
+            'email_to':
+            form.data.get('email_to', None),
+            'filename':
+            form.data.get('filename', 'unit_report'),
+            'live':
+            form.data.get('live', None),
+            'latest_revision_created_at':
+            form.data.get('latest_revision_created_at', None),
         }
-        options['all'] = not(bool(options['live']))
+        options['all'] = not (bool(options['live']))
         if form.is_valid():
             unit_report = WagtailUnitsReport(
-                sync_report = False,
-                unit_report = True,
-                **options
+                sync_report=False, unit_report=True, **options
             )
             virtual_workbook = save_virtual_workbook(unit_report.workbook())
-            response = HttpResponse(virtual_workbook, content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
-            response['content-disposition'] = 'attachment; filename="' + options['filename'] + '.xlsx"'
+            response = HttpResponse(
+                virtual_workbook,
+                content_type=
+                'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+            )
+            response['content-disposition'] = 'attachment; filename="' + \
+                options['filename'] + '.xlsx"'
             return response
         else:
-            return render(request, 'units/unit_reporting_form.html', {'form': form})
+            return render(
+                request, 'units/unit_reporting_form.html', {'form': form}
+            )
     else:
         form = UnitReportingForm({'live': True, 'filename': 'unit_report'})
-    return render(request, 'units/unit_reporting_form.html', {
-        'form': form
-    })
-
+    return render(request, 'units/unit_reporting_form.html', {'form': form})
