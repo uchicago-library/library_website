662daa87b35332a342270119755034b017c9f235
Brad Busenius <bradb@neomailbox.ch>
2021-07-22 11:43:52 -0500

    Update CGIMail form config to use a site relative path instead of an absolute URL endpoint. Fixes issue #541.

--- library_website/settings/base.py
+++ library_website/settings/base.py
@@ -517,7 +517,7 @@ LIBCAL_CREDENTIALS = {
 }
 
 # CGIMail Forms
-CGI_MAIL_SERVICE = 'https://www.lib.uchicago.edu/cgi-bin/cgimail/cgimail'
+CGI_MAIL_SERVICE = '/cgi-bin/cgimail/cgimail'
 ITEM_SERVLET = 'https://forms2.lib.uchicago.edu/lib/searchform/itemServlet.php?format=json'
 SPRINGSHARE_PRIVACY_POLICY = 'https://springshare.com/privacy.html'
 
