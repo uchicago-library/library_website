4ef90bcc8ade8bbe857bfeb94677bd6916291934
Irina Stamm <irina@xibalba.lib.uchicago.edu>
2022-11-11 17:42:21 -0600

    some functional abstraction

--- library_website/views.py
+++ library_website/views.py
@@ -4,8 +4,48 @@ from wagtail.models import Site
 
 
 def library_404_view(request, exception=Http404):
+
     site_name = Site.find_for_request(request).site_name
-    if site_name == "Public":
-        return render(request, '404_lib.html')
-    else:
-        return render(request, '404_loop.html')
+
+    class Link:
+
+        def __init__(self, text, url):
+            self.text = text
+            self.url = url
+
+    def context_404(site_name):
+        if site_name == "Public":
+            return {
+                "extention":
+                "base/public_base.html",
+                "links": [
+                    Link(
+                        "Ask a Librarian",
+                        "https://www.lib.uchicago.edu/research/help/ask-librarian/"
+                    ),
+                    Link(
+                        "Library Digital Collections",
+                        "https://www.lib.uchicago.edu/collex/"
+                    ),
+                    Link("Library Homepage", "https://www.lib.uchicago.edu/")
+                ],
+                "helptext":
+                "Let us know that we have a missing page. We will do our best to rectify the issue."
+            }
+        else:
+            return {
+                "extention":
+                "intranethome/intranet_home_page.html",
+                "links": [
+                    Link(
+                        "Return to Loop Homepage",
+                        "https://loop.lib.uchicago.edu/"
+                    )
+                ],
+                "helptext":
+                "Let us know that we have a missing page. Contact intranet@lib.uchicago.edu and we will do our best to rectify the issue."
+            }
+
+    context = context_404(site_name)
+
+    return render(request, '404.html', context)
