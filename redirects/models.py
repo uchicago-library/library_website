cac615201a1ee5996742dddcfaa70a3a6e4ebb51
Irina Stamm <irina@xibalba.lib.uchicago.edu>
2022-12-09 17:46:57 -0600

    updated the loop redirect page

--- redirects/models.py
+++ redirects/models.py
@@ -39,14 +39,14 @@ class LoopRedirectPage(BasePage, LinkFields):
     """
     subpage_types = []
 
-    redirect_url = models.URLField(
-        max_length=200, default="https://loop.lib.uchicago.edu/"
-    )
-
     content_panels = Page.content_panels + [
         MultiFieldPanel(
-            [FieldPanel('redirect_url'),
-             PageChooserPanel('link_page')]
+            [
+                PageChooserPanel('link_page'),
+                FieldPanel('link_external'),
+                FieldPanel('link_document'),
+            ],
+            heading='Redirect to'
         )
     ] + BasePage.content_panels
 
@@ -54,4 +54,4 @@ class LoopRedirectPage(BasePage, LinkFields):
         """
         Override the serve method to create a redirect.
         """
-        return redirect(self.redirect_url, permanent=True)
+        return redirect(self.link, permanent=True)
