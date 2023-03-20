625aaba1dbe0e285ca8d3729e922bcd90a5c4d3b
Matt Teichman <teichman@uchicago.edu>
2023-03-20 17:18:36 -0500

    adding getItem to wrangle_json, still in progress, untested

--- lib_collections/wrangle_json.py
+++ lib_collections/wrangle_json.py
@@ -13,22 +13,34 @@ class CleanData():
 
     class KeyValue():
 
-        def key_value(keyField, valField, data):
+        def adjacent_key_value(keyField, valField, data):
             bindings = data["results"]["bindings"]
             def each_pair(var):
                 return (var[keyField]["value"], var[valField]["value"])
             return OrderedDict([ each_pair(pred) for pred in bindings ])
 
-    key_value = KeyValue.key_value
+        def downward_key_value(data):
+            def each_pair(k,v):
+                return (k, v["value"].split("|"))
+            def nonempty(data):
+                v = data["value"]
+                unavailable = '(:unav)'
+                return v and v != unavailable
+            def each_binding(dct):
+                return OrderedDict([ each_pair(k,v) for (k,v) in dct.items() if nonempty(v) ])
+            bindings = data["results"]["bindings"]
+            return [ each_binding(b) for b in bindings ]
+
+    adjacent_key_value = KeyValue.adjacent_key_value
     
     def getBrowseListContributors(data):
-        return CleanData.key_value("o", "s", data)
+        return CleanData.adjacent_key_value("o", "s", data)
 
     def getBrowseListLocations(data):
-        return CleanData.key_value("prefLabel", "spatial", data)
+        return CleanData.adjacent_key_value("prefLabel", "spatial", data)
 
     def getBrowseListLanguages(data):
-        return CleanData.key_value("code", "prefLabel", data)
+        return CleanData.adjacent_key_value("code", "prefLabel", data)
 
     class StraightUpList():
 
@@ -38,26 +50,18 @@ class CleanData():
 
     straight_up_list = StraightUpList.straight_up_list
 
+    class CollectionObject():
+
+        def obj():
+            pass
+
     def getBrowseListDates(data):
         return CleanData.straight_up_list("date", data)
 
+itemStuff = open_json("./getItem.json")
+obj = CleanData.CollectionObject.obj
+
 class URLs():
-
-    class QueryStrings():
-
-        def getBrowseListContributors(collection="mlc"):
-            return { "collection" : collection }
-
-        def getBrowseListLocations(collection="mlc"):
-            return { "collection" : collection }
-
-        def getBrowseListLanguages(collection="mlc"):
-            return { "collection" : collection }
-
-        def getBrowseListDates(collection="mlc"):
-            return { "collection" : collection }
-    
-class Api():
 
     ML_HOST = "http://marklogic.lib.uchicago.edu"
     ML_PORT = 8031
@@ -79,16 +83,40 @@ class Api():
             return "".join(parts)
 
         def assemble_url_prefix(collection, api_name):
-            return Api.BaseURL.assemble_url_prefix_full(
-                Api.ML_HOST,
+            return URLs.BaseURL.assemble_url_prefix_full(
+                URLs.ML_HOST,
                 collection,
                 api_name,
-                Api.ML_PORT,
-                Api.ML_PATH
+                URLs.ML_PORT,
+                URLs.ML_PATH
             )
 
     assemble_url_prefix = BaseURL.assemble_url_prefix
 
+    class QueryStrings():
+
+        def getBrowseListContributors(collection="mlc"):
+            return { "collection" : collection }
+
+        def getBrowseListLocations(collection="mlc"):
+            return { "collection" : collection }
+
+        def getBrowseListLanguages(collection="mlc"):
+            return { "collection" : collection }
+
+        def getBrowseListDates(collection="mlc"):
+            return { "collection" : collection }
+
+    class MakeURL():
+
+        def make_api_string(collection, api_name, params):
+            query_string = urllib.parse.urlencode(params)
+            url_prefix = URLs.assemble_url_prefix(collection, api_name)
+            return url_prefix + query_string
+
+    make_api_string = MakeURL.make_api_string
+    
+class Api():
 
     class URLGet():
 
@@ -97,39 +125,33 @@ class Api():
             data = response.json()
             return func(data)
 
-        def make_api_string(collection, api_name, params):
-            query_string = urllib.parse.urlencode(params)
-            url_prefix = Api.assemble_url_prefix(collection, api_name)
-            return url_prefix + query_string
-
     pull_from_url = URLGet.pull_from_url
-    make_api_string = URLGet.make_api_string
 
     def getBrowseListContributors(collection="mlc"):
-        params = Api.QueryStrings.getBrowseListContributors(collection)
+        params = URLs.QueryStrings.getBrowseListContributors(collection)
         cleanup = CleanData.getBrowseListContributors
-        url = Api.make_api_string(collection, "getBrowseListContributors", params)
+        url = URLs.make_api_string(collection, "getBrowseListContributors", params)
         data = Api.pull_from_url(url, cleanup)
         return data
         
     def getBrowseListLocations(collection="mlc"):
-        params = Api.QueryStrings.getBrowseListLocations(collection)
+        params = URLs.QueryStrings.getBrowseListLocations(collection)
         cleanup = CleanData.getBrowseListLocations
-        url = Api.make_api_string(collection, "getBrowseListLocations", params)
+        url = URLs.make_api_string(collection, "getBrowseListLocations", params)
         data = Api.pull_from_url(url, cleanup)
         return data
 
     def getBrowseListLanguages(collection="mlc"):
-        params = Api.QueryStrings.getBrowseListLanguages(collection)
+        params = URLs.QueryStrings.getBrowseListLanguages(collection)
         cleanup = CleanData.getBrowseListLanguages
-        url = Api.make_api_string(collection, "getBrowseListLanguages", params)
+        url = URLs.make_api_string(collection, "getBrowseListLanguages", params)
         data = Api.pull_from_url(url, cleanup)
         return data
 
     def getBrowseListDates(collection="mlc"):
-        params = Api.QueryStrings.getBrowseListDates(collection)
+        params = URLs.QueryStrings.getBrowseListDates(collection)
         cleanup = CleanData.getBrowseListDates
-        url = Api.make_api_string(collection, "getBrowseListDates", params)
+        url = URLs.make_api_string(collection, "getBrowseListDates", params)
         data = Api.pull_from_url(url, cleanup)
         return data
 
