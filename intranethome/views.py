488a2d3850a57534edfa23f6f171bf514cab25d8
Irina Stamm <irina@xibalba.lib.uchicago.edu>
2023-06-07 10:39:11 -0500

    making the display pretty

--- intranethome/views.py
+++ intranethome/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from pathlib import Path
 import json
+import re
 
 def mail_aliases_view(request):
     f = open("intranethome/keith_json.txt", "r")
@@ -25,6 +26,36 @@ def mail_aliases_view(request):
         for email in python_text[key][0]:
             if 'email' in email:
                 python_text[key][0][integer] = email['email']
+
+                ### splitting up formatted emails and people 
+                ### putting the email itself into the last slot
+                
+                location = {}
+                tringle_brackets = re.search("(.*)<(.*)>", email['email'])
+                parentheses = re.search("(.*)\s*\((.*)\)", email['email'])
+                if (not tringle_brackets and not parentheses):
+                    plain_email = re.search(".*@.*", email['email'])
+                
+                if(tringle_brackets):
+                    location["triangle_brackets"] = []
+                    location["triangle_brackets"].append(tringle_brackets[1].strip())
+                    location["triangle_brackets"].append(tringle_brackets[2])
+                    location["triangle_brackets"].append(tringle_brackets[0])
+                
+                elif(parentheses):
+                    location["parentheses"] = []
+                    location["parentheses"].append(parentheses[2])
+                    location["parentheses"].append(parentheses[1])
+                    location["parentheses"].append(parentheses[0])
+
+                elif(plain_email):
+                    location["plain_email"] = (plain_email[0])
+
+                else:
+                    location["local"] = (email['email'])
+            
+                python_text[key][0][integer] = location
+
             integer += 1
         
         cleaned_data[key] = python_text[key][0]
