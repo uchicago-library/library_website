e84bcde70f9bad07f3b77fdad291fad0dc176372
Irina Stamm <irina@xibalba.lib.uchicago.edu>
2023-06-30 09:15:33 -0500

    only loop permissions

--- intranethome/views.py
+++ intranethome/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 import json
 import re
 from django.http import Http404
+from wagtail.models import Site
 
 
 # purpose of function: reading json text and converting to python
@@ -174,6 +175,12 @@ def format_splitting(email):
     return parsed_email
 
 def mail_aliases_view(request):
+
+    loop_homepage = Site.objects.get(site_name='Loop').root_page
+    if not has_permission(request.user, get_required_groups(loop_homepage)):
+        return redirect_users_without_permissions(
+            loop_homepage, request, None, None
+        )
 
     file_data = reading_and_converting("/data/web/aliases/aliases.json")
 
