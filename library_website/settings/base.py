f8b0efe2f044165f6625112c8310beac84e53233
Brad Busenius <bbusenius@uchicago.edu>
2023-08-01 12:23:05 -0500

    Synced the CORS settings to match what django-cors-headers now expects.

--- library_website/settings/base.py
+++ library_website/settings/base.py
@@ -243,8 +243,8 @@ WAGTAILSEARCH_BACKENDS = {
 # Settings for cross origin requests
 # Documentation and settings:
 # https://github.com/OttoYiu/django-cors-headers
-CORS_ORIGIN_ALLOW_ALL = False
-CORS_ORIGIN_WHITELIST = (
+CORS_ALLOW_ALL_ORIGINS = False
+CORS_ALLOWED_ORIGINS = (
     'https://catalog.lib.uchicago.edu',
     'https://catalogtest.lib.uchicago.edu',
     'https://dldc1.lib.uchicago.edu',
