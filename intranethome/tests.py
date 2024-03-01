d50873639096f6f3b6e4363c7857a1f19e81a9d0
Matt Teichman <teichman@uchicago.edu>
2024-03-01 13:53:03 -0600

    oops, forgot to also remove imports

--- intranethome/tests.py
+++ intranethome/tests.py
@@ -1,9 +1,3 @@
 from django.test import SimpleTestCase
 
-from intranethome.views import (
-    format_splitting,
-    reading_and_converting,
-    uniforming_into_list_of_dict,
-)
-
 # TODO: write new tests to reflect the updated code in views.py
