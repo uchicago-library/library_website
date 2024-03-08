b73726a7252728f645f57e181f18b0242e582133
Brad Busenius <bbusenius@uchicago.edu>
2024-03-08 16:39:18 -0600

    Reverted previous patch and config changes to mirror new changes from LiveWhale.

--- events/utils.py
+++ events/utils.py
@@ -25,7 +25,7 @@ def get_xml_from_feed(feed):
 
 def get_entries_from_events_uchicago(x):
     entries = []
-    for entry in x.find('channel').find('div').findall('item'):
+    for entry in x.find('channel').findall('item'):
         content = ''
         html = entry.find('description').itertext()
         for tag in html:
