c0d183ef00c6436283a39f3fb6cc86ab8ecf06de
Vitor Gonçalves <vitorg@uchicago.edu>
2025-04-25 13:44:27 -0500

    fix categories and click position

--- base/static/base/js/jquery.track-everything.js
+++ base/static/base/js/jquery.track-everything.js
@@ -28,8 +28,8 @@
     // Function to determine event parameters based on link context.
     function getEventParameters(link) {
         const params = {
-            event_category: null,
-            event_subcategory: null,
+            event_category: link.getAttribute('data-ga-category'),
+            event_subcategory: link.getAttribute('data-ga-subcategory'),
             event_label: link.getAttribute('data-ga-label') ||
                 link.getAttribute('aria-label') ||
                 (link.textContent.replace(/\s+/g, ' ').trim()) ||
@@ -39,48 +39,62 @@
             click_position: null,
         };
 
-        // Determine category, subcategory, and click_position based on link context.
+        // Determine category, subcategory, based on link context.
         if (link.closest('#global-navbar')) { // main navbar
-            params.event_category = 'navigation';
+            params.event_category = params.event_category || 'navigation';
             const listParent = link.closest('ul, ol');
-            params.event_subcategory = link.getAttribute('data-ga-subcategory') ||
+            params.event_subcategory = params.event_subcategory ||
                 (listParent ? listParent.getAttribute('aria-labelledby') : null) ||
                 params.label || 'navigation';
 
         } else if (link.closest('#navbar-right')) { // shortcuts
-            params.event_category = 'navbar-shortcuts';
-            params.event_subcategory = link.getAttribute('data-ga-subcategory') || 'shortcuts';
+            params.event_category = params.event_category || 'navbar-shortcuts';
+            params.event_subcategory = params.event_subcategory || 'navbar-shortcuts';
 
         } else if (link.closest('footer')) { // footer
-            params.event_category = 'footer';
+            params.event_category = params.event_category || 'footer';
             const listParent = link.closest('ul, ol');
-            params.event_subcategory = link.getAttribute('data-ga-subcategory') ||
+            params.event_subcategory = params.event_subcategory ||
                 (listParent ? listParent.getAttribute('aria-labelledby') : null) || 'footer';
 
-        } else if (link.closest('.widget, [id^="widget"]')) { // any widget
-            params.event_category = 'widget';
-            const widgetParent = link.closest('.widget, [id^="widget"]');
-            params.event_subcategory = link.getAttribute('data-ga-subcategory') ||
-                (widgetParent?.id.startsWith('widget') ? widgetParent.id : null) || null;
+        } else if (link.closest('.widget, [id*="widget"]')) { // any widget
+            params.event_category = params.event_category || 'widget';
+            const widgetParent = link.closest('.widget, [id*="widget"]');
+            params.event_subcategory = params.event_subcategory ||
+                (widgetParent?.id.includes('widget') ? widgetParent.id : null) || null;
 
-        } else if (link.closest('[class^="sidebar"], [role="complementary"], [id^="sidebar"]')) { // any sidebar
+        } else if (link.closest('[class^="sidebar"], [role="complementary"], [id*="sidebar"]')) { // any sidebar
             params.event_category = 'sidebar';
-            const sidebarParent = link.closest('[class^="sidebar"], [id^="sidebar"]');
+            const sidebarParent = link.closest('[class^="sidebar"], [id*="sidebar"]');
             params.event_subcategory = link.getAttribute('data-ga-subcategory') ||
-                (sidebarParent?.id.startsWith('sidebar') ? sidebarParent.id : '') ||
-                (sidebarParent?.className.split(' ').find(c => c.startsWith('sidebar')) || 'sidebar-link') || null;
+                (sidebarParent?.id.includes('sidebar') ? sidebarParent.id : '') ||
+                (sidebarParent?.className.split(' ').find(c => c.includes('sidebar')) || null);
 
         } else { // main content
-            params.event_category = 'main';
-            params.event_subcategory = link.closest('[id]').getAttribute('id');
-            if (link.getAttribute('data-ga-position')) {
+            params.event_category = params.event_category || 'main';
+            params.event_subcategory = params.event_category || link.closest('[id]').getAttribute('id');
+        }
+
+        // Get link position if in a list
+        if (link.getAttribute('data-ga-position')) {
+            params.event_subcategory = params.event_subcategory || 'list';
+            params.click_position = parseInt(link.getAttribute('data-ga-position'), 10);
+        } else {
+            const parentLi = link.closest('li');
+            if (parentLi) {
                 params.event_subcategory = params.event_subcategory || 'list';
-                params.click_position = parseInt(link.getAttribute('data-ga-position'), 10);
+                params.click_position = Array.from(parentLi.parentElement.children).indexOf(parentLi) + 1;
             } else {
-                const parentLi = link.closest('li');
-                if (parentLi) {
-                    params.event_subcategory = 'list';
-                    params.click_position = Array.from(parentLi.parentElement.children).indexOf(parentLi) + 1;
+                const parentNews = link.closest('.news-wrap');
+                if (parentNews) {
+                    params.event_subcategory = params.event_subcategory || 'news-list';
+                    params.click_position = Array.from(parentNews.children).indexOf(link.closest('.newsblock')) + 1;
+                } else {
+                    const parentNews = link.closest('.news-stories');
+                    if (parentNews) {
+                        params.event_subcategory = params.event_subcategory || 'news-list';
+                        params.click_position = Array.from(parentNews.children).indexOf(link.closest('article')) + 1;
+                    }
                 }
             }
         }
@@ -121,19 +135,20 @@
     function handleLinkClick(event) {
         event.preventDefault(); // Prevents default behavior like following links
         event.stopPropagation(); // Stops event from bubbling up
-        const target = event.target.closest('a');
+        const target = event.target.closest('a, button, input');
         const eventName = getEventName(target);
         if (eventName) {
-            const eventParams = getEventParameters(target);
+            const ep = getEventParameters(target);
             // gtag('event', eventName, eventParams);
-            console.log("Event Name: " + eventName);
-            console.log("Event params: " + eventParams);
+            console.log("Event Name: " + eventName + "\n" + ep.event_category + "\n" + ep.event_subcategory + "\n" + ep.event_label + "\n" + ep.click_position);
         } else {
             console.log("No click event. Event name: " + eventName)
+            console.log("No click event. Event name: " + eventName + ". event.target.tagName: " + event.target.tagName)
         }
     }
 
     // Attach a single event listener to the document body using event delegation.
-    document.body.addEventListener('click', debounce(handleLinkClick, 200));
+    // document.body.addEventListener('click', debounce(handleLinkClick, 200));
+    document.body.addEventListener('click', handleLinkClick);
 })();
 
