c9cd4848c1c9d82b7d9f02a728d7f77f8a279553
Matt Teichman <teichman@uchicago.edu>
2025-08-28 15:52:18 -0500

    remove validation on the proxy linker

--- public/static/public/js/proxy-linker.js
+++ public/static/public/js/proxy-linker.js
@@ -1,9 +1,3 @@
-const isProxyLink = (link) => {
-    url = URL.parse(link);
-    hostname = url.hostname ? url.hostname : null;
-    return hostname == "proxy-redirector-test.lib.uchicago.edu";
-};
-
 const proxify = (link) => {
     const uri = encodeURIComponent (link.trim());
     const scheme = "https";
@@ -11,23 +5,20 @@ const proxify = (link) => {
     const route = "login";
     const querystring = "url=" + uri;
     const output = scheme + "://" + hostname + "/" + route + "?" + querystring;
-    return isProxyLink (link) ? link : output;
+    return output;
 };
 
-const proxifyForm = (form) => {
+const proxifyForm = (form, pdisplay) => {
+    pbutton = document.getElementById("pbutton");
+    pbutton.classList.remove("btn-secondary");
+    pbutton.classList.add("btn-primary");
     const link = form.value;
     const proxified = proxify (link);
     document.getElementById ("copy_button").hidden = false;
-    document.getElementById ("proxied_link").innerHTML = "proxified link: " + proxified;
+    pdisplay.value = link ? proxified : pdisplay.value;
 };
 
 const copyLink = (form) => {
     form.select ();
     document.execCommand ("copy");
-};
-
-const displayText = (newText) => {
-    if (newText.trim()) {
-	document.getElementById ("original_link").innerHTML = "original link: " + newText;
-    } else {};
 };
