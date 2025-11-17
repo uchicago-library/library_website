19a6bdd1ad94497cdc94eb986f45e7577f0e3fb4
Vitor Gonçalves <vitorg@uchicago.edu>
2025-11-17 10:16:36 -0600

    don't interfere with lightbox or holdings links.

--- base/static/base/js/jquery.track-everything.js
+++ base/static/base/js/jquery.track-everything.js
@@ -584,6 +584,11 @@
                 return;
             }
 
+            if (target.hasAttribute('data-toggle') || target.classList.contains('holdingslink')) {
+                // Don't interfere with the ekkolightbox or with SFX FindIt holdings links.
+                return;
+            }
+
             if (href && !isNewTab && !(eventName === 'tab' && href && href.startsWith('#'))) {
                 event.preventDefault(); // delay navigation just slightly
                 setTimeout(() => {
