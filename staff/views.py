3ef67222124b4a43a44fddbea35fa0e7ea79ec12
Brad Busenius <bbusenius@users.noreply.github.com>
2025-11-29 12:19:26 -0600

    Add supervisors field to staff_api JSON endpoint (#922)
    * Add supervisors field to staff_api JSON endpoint
    
    Added get_serialized_supervisors() method to return supervisor details
    (name, cnetid, position_title) and exposed it via the staff_api endpoint
    to support HR workflows.
    
    - Added get_serialized_supervisors() method to StaffPage model
    - Added supervisors field to api_fields list
    - Added test coverage for serialized supervisors with multiple scenarios
    
    * Add nocache_page decorator to staff_api
    
    * Make the staff_api accessible to authenticated users with tokens in the admin
    
    * Simplify staff_api and remove HTTP overhead

--- staff/views.py
+++ staff/views.py
@@ -1,25 +1,29 @@
-import requests
-
 from base.wagtail_hooks import (
-    get_required_groups, has_permission, redirect_users_without_permissions
+    get_required_groups,
+    has_permission,
+    redirect_users_without_permissions,
 )
 from django.core.exceptions import PermissionDenied
 from django.http import JsonResponse
 from django.shortcuts import render
 from intranetunits.models import IntranetUnitsPage
+from library_website.api import api_router
 from public.models import LocationPage
-from rest_framework.authentication import (
-    SessionAuthentication, TokenAuthentication
-)
+from rest_framework.authentication import SessionAuthentication, TokenAuthentication
 from rest_framework.decorators import (
-    api_view, authentication_classes, permission_classes
+    api_view,
+    authentication_classes,
+    permission_classes,
 )
 from rest_framework.permissions import IsAuthenticated
-from staff.models import StaffPage, StaffPageSubjectPlacement
 from subjects.models import Subject
 from units.views import get_staff_pages_for_unit
-from wagtail.models import Site
+from wagtail.api.v2.views import PagesAPIViewSet
 from wagtail.images.models import Image
+from wagtail.models import Site
+from wagtailcache.cache import nocache_page
+
+from staff.models import StaffPage, StaffPageSubjectPlacement
 
 
 def staff(request):
@@ -31,9 +35,7 @@ def staff(request):
 
     loop_homepage = Site.objects.get(site_name='Loop').root_page
     if not has_permission(request.user, get_required_groups(loop_homepage)):
-        return redirect_users_without_permissions(
-            loop_homepage, request, None, None
-        )
+        return redirect_users_without_permissions(loop_homepage, request, None, None)
 
     staff_pages = []
     flat_units = []
@@ -44,24 +46,24 @@ def staff(request):
         elif library:
             staff_pages = StaffPage.get_staff_by_building(library)
         else:
-            staff_pages = StaffPage.objects.live().order_by(
-                'last_name', 'first_name'
-            )
+            staff_pages = StaffPage.objects.live().order_by('last_name', 'first_name')
 
         if subject:
             subject_pk = Subject.objects.get(name=subject).pk
-            staff_pks = StaffPageSubjectPlacement.objects.filter(
-                subject=subject_pk
-            ).values_list('page', flat=True).distinct()
+            staff_pks = (
+                StaffPageSubjectPlacement.objects.filter(subject=subject_pk)
+                .values_list('page', flat=True)
+                .distinct()
+            )
 
             if staff_pages:
-                staff_pages = staff_pages.filter(
-                    pk__in=staff_pks
-                ).order_by('last_name', 'first_name')
+                staff_pages = staff_pages.filter(pk__in=staff_pks).order_by(
+                    'last_name', 'first_name'
+                )
             else:
-                staff_pages = StaffPage.objects.filter(
-                    pk__in=staff_pks
-                ).order_by('last_name', 'first_name')
+                staff_pages = StaffPage.objects.filter(pk__in=staff_pks).order_by(
+                    'last_name', 'first_name'
+                )
 
         if query:
             if staff_pages:
@@ -70,8 +72,10 @@ def staff(request):
                 staff_pages = StaffPage.objects.live().search(query)
 
         if not department and not library and not subject and not query:
-            staff_pages = StaffPage.objects.live().order_by('title').order_by(
-                'last_name', 'first_name'
+            staff_pages = (
+                StaffPage.objects.live()
+                .order_by('title')
+                .order_by('last_name', 'first_name')
             )
     elif view == 'department':
         if query:
@@ -96,44 +100,37 @@ def staff(request):
         flat_units = sorted(flat_units, key=lambda k: k['title'])
 
     # Subjects
-    subject_pks = StaffPageSubjectPlacement.objects.all().values_list(
-        'subject', flat=True
-    ).distinct()
-    subjects = Subject.objects.filter(pk__in=subject_pks
-                                      ).values_list('name', flat=True)
+    subject_pks = (
+        StaffPageSubjectPlacement.objects.all()
+        .values_list('subject', flat=True)
+        .distinct()
+    )
+    subjects = Subject.objects.filter(pk__in=subject_pks).values_list('name', flat=True)
 
     default_image = Image.objects.get(title="Default Placeholder Photo")
 
     return render(
-        request, 'staff/staff_index_page.html', {
-            'default_image':
-            default_image,
-            'department':
-            department,
-            'flat_intranet_units':
-            flat_units,
+        request,
+        'staff/staff_index_page.html',
+        {
+            'default_image': default_image,
+            'department': department,
+            'flat_intranet_units': flat_units,
             'libraries': [
-                str(p)
-                for p in LocationPage.objects.live().filter(is_building=True)
+                str(p) for p in LocationPage.objects.live().filter(is_building=True)
             ],
-            'library':
-            library,
-            'query':
-            query,
-            'subject':
-            subject,
-            'subjects':
-            subjects,
-            'subjects':
-            subjects,
-            'staff_pages':
-            staff_pages,
-            'view':
-            view
-        }
+            'library': library,
+            'query': query,
+            'subject': subject,
+            'subjects': subjects,
+            'subjects': subjects,
+            'staff_pages': staff_pages,
+            'view': view,
+        },
     )
 
 
+@nocache_page
 @api_view(['GET'])
 @authentication_classes((SessionAuthentication, TokenAuthentication))
 @permission_classes((IsAuthenticated,))
@@ -143,14 +140,13 @@ def staff_api(request):
     provide a user token in the headers. In order to make tokenization work,
     the Django Rest Framework requires us to use the @api_view decorator.
     Since we can't directly apply this to the Wagtail API (built on Django
-    Rest Framework) we apply it here and pass the request on to the default
-    Wagtail API. We return the response that Wagtail API gives us. To edit
-    or add fields to this API go to the StaffPage model.
+    Rest Framework) we apply it here and call the Wagtail API view directly.
+    To edit or add fields to this API go to the StaffPage model.
 
-    This view only passes the request to the Wagtail pages API and returns
-    the output. The view will only be rendered if the request headers contain
-    a token. Any user can have a token, however, we do not create them by
-    default.
+    This view calls the Wagtail pages API view directly without making an
+    HTTP request. The view will only be rendered if the request headers contain
+    a token or if the user is session authenticated. Any user can have a token,
+    however, we do not create them by default.
 
     Create and view a token for a given user:
     from rest_framework.authtoken.models import Token
@@ -167,17 +163,23 @@ def staff_api(request):
     """
 
     if request.method == 'GET':
-        copy = request.GET.copy()
-        params = copy.urlencode()
-        scheme = request.is_secure() and 'https' or 'http'
-        base_url = scheme + '://' + request.get_host()
-        rest_url = base_url + '/api/v2/pages/?' + params
+        # Get the Wagtail Pages API viewset and create a view instance for listing
+        # Note: Wagtail uses 'listing_view' instead of the standard DRF 'list' action
+        view_instance = PagesAPIViewSet.as_view({'get': 'listing_view'})
 
-        try:
-            token = request.META['HTTP_AUTHORIZATION']
-            rest_headers = {'Authorization': token}
-            rest_response = requests.get(rest_url, headers=rest_headers)
-            return JsonResponse(rest_response.json())
-        except (KeyError):
-            raise PermissionDenied
+        # Get the underlying Django request and add the router reference
+        # The @api_view decorator wraps the HttpRequest in a DRF Request,
+        # so we need to use request._request to get the original Django request.
+        # The Wagtail API requires a 'wagtailapi_router' attribute on the request.
+        django_request = request._request
+        django_request.wagtailapi_router = api_router
+
+        # Call the view with the prepared request
+        # Authentication has already been handled by @authentication_classes,
+        # so django_request.user is properly set to the authenticated user.
+        response = view_instance(django_request)
+
+        # Return the response data as JSON
+        return JsonResponse(response.data)
+
     raise PermissionDenied
