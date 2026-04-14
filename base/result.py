ec9c0cc88f44cae8fe6674ce3967e6689bb5afea
Matt Teichman <teichman@uchicago.edu>
2026-04-14 15:43:07 -0500

    oops, forgot to commit result.py

---
+++ base/result.py
@@ -0,0 +1,51 @@
+############ monadic error handling utility functions ################
+
+
+def ok(x):
+    return { "ok": x }
+
+
+def error(msg):
+    return { "error": msg }
+
+
+class InvalidResult(Exception):
+    def __init__(self, msg="invalid result value "):
+        self.msg = msg
+        super().__init__(self.msg)
+
+
+def rmap(f, result):
+    match result:
+        case { "ok": x }:
+            return ok(f(x))
+        case { "error": msg }:
+            return { "error": msg }
+        case other:
+            msg = "invalid result value: %s" % str(other)
+            raise InvalidResult(msg)
+
+
+def product(result1, result2):
+    match (result1, result2):
+        case ({ "ok": ok1 }, {"ok": ok2 }):
+            return { "ok": (ok1, ok2) }
+        case ({ "error": msg }, _):
+            return { "error": msg }
+        case (_, { "error": msg }):
+            return { "error": msg }
+        case (one, two):
+            msg = "invalid results: %s and %s" % (str(one),
+                                                   str(two))
+            raise InvalidResult(msg)
+
+
+def bind(result, k):
+    match result:
+        case { "ok": x }:
+            return k(x)
+        case { "error": msg }:
+            return { "error": msg }
+        case other:
+            msg = "invalid result value: %s" % str(other)
+            raise InvalidResult(msg)
