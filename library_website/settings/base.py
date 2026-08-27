baa04ec6cd7fa904824fdbed43d09c071d108f2c
Matt Teichman <teichman@uchicago.edu>
2026-08-27 14:30:46 -0500

    ah; also no longer need the port number

--- library_website/settings/base.py
+++ library_website/settings/base.py
@@ -506,8 +506,6 @@ MARKLOGIC_LDR_URL = "https://%s.%s/%s" % (
     MARKLOGIC_LDR_ROUTE,
 )
 
-MARKLOGIC_FINDINGAIDS_PORT = 8011
-
 E_FINDING_AIDS_URL = "http://www.lib.uchicago.edu/e/scrc/findingaids/view.php"
 E_FINDING_AIDS_QUERY_STRING = "?eadid="
 E_FINDING_AIDS = E_FINDING_AIDS_URL + E_FINDING_AIDS_QUERY_STRING
